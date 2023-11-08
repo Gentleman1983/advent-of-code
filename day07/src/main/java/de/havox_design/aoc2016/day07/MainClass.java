fe77560ed192feed187bf2f1fdb77d7420a80ff6
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-11-08 21:43:54 +0100

    Updated project template

--- day07/src/main/java/de/havox_design/aoc2016/day07/MainClass.java
+++ day07/src/main/java/de/havox_design/aoc2016/day07/MainClass.java
@@ -6,7 +6,7 @@ public class MainClass {
     private static final Logger LOGGER = Logger.getLogger(MainClass.class.getName());
 
     public static void main(String[] args) {
-        LOGGER.info("Solution 1: 13");
-        LOGGER.info("Solution 2: 23");
+        LOGGER.info("Part 1: 13");
+        LOGGER.info("Part 2: 23");
     }
 }
