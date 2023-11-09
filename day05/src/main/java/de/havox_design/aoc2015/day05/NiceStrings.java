95f37c9617113e5f156ae69b9e1212b8227420c5
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-11-09 17:32:00 +0100

    Added some functionality

--- day05/src/main/java/de/havox_design/aoc2015/day05/NiceStrings.java
+++ day05/src/main/java/de/havox_design/aoc2015/day05/NiceStrings.java
@@ -1,2 +1,59 @@
-package de.havox_design.aoc2015.day05;public class NiceStrings {
+package de.havox_design.aoc2015.day05;
+
+import de.havox_design.aoc2015.utils.DataReader;
+
+import java.util.List;
+
+public class NiceStrings {
+    private final List<String> input;
+
+    public NiceStrings(String fileName) {
+        input = readData(fileName);
+    }
+
+    public static int solvePart1(String fileName) {
+        NiceStrings instance = new NiceStrings(fileName);
+        return instance.solvePart1();
+    }
+
+    public static int solvePart2(String fileName) {
+        NiceStrings instance = new NiceStrings(fileName);
+        return instance.solvePart2();
+    }
+
+    public int solvePart1() {
+        int numberNiceStrings = 0;
+
+        for (String string : input) {
+            if (
+                    countVowels(string) >= 3 &&
+                            containsDoubleLetter(string) &&
+                            !containsForbiddenStrings(string)
+            ) {
+                numberNiceStrings++;
+            }
+        }
+
+        return numberNiceStrings;
+    }
+
+    public int solvePart2() {
+        return 0;
+    }
+
+    private int countVowels(String string) {
+        return 0;
+    }
+
+    private boolean containsDoubleLetter(String string) {
+        return false;
+    }
+
+    private boolean containsForbiddenStrings(String string) {
+        return false;
+    }
+
+    private List<String> readData(String fileName) {
+        return DataReader.readData(fileName, MainClass.class);
+    }
 }
--- day05/src/test/java/de/havox_design/aoc2015/day05/Day05Test.java
+++ day05/src/test/java/de/havox_design/aoc2015/day05/Day05Test.java
@@ -1,6 +1,12 @@
 package de.havox_design.aoc2015.day05;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class Day05Test {
 
@@ -8,5 +14,22 @@ class Day05Test {
     @Test
     void testMainClass() {
         MainClass.main(new String[0]);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getDataForPart1")
+    void testPart1(String fileName, int expectedNumberOfNiceStrings) {
+        Assertions.assertEquals(expectedNumberOfNiceStrings, NiceStrings.solvePart1(fileName));
+    }
+
+    private static Stream<Arguments> getDataForPart1() {
+        return Stream.of(
+                Arguments.of("part1sample1.txt", 1),
+                Arguments.of("part1sample2.txt", 1),
+                Arguments.of("part1sample3.txt", 0),
+                Arguments.of("part1sample4.txt", 0),
+                Arguments.of("part1sample5.txt", 0),
+                Arguments.of("part1sample6.txt", 2)
+        );
     }
 }
