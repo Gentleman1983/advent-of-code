da85bdf545cefa1ac2fe4e1a280a3853ceb884bd
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-11-26 00:07:03 +0100

    Solved day 16 part 1

---
+++ day16/src/main/kotlin/de/havox_design/aoc2015/day16/FindAuntSue.kt
@@ -0,0 +1,48 @@
+package de.havox_design.aoc2015.day16
+
+import java.util.stream.Collectors
+
+class LogicGates(private var filename: String) {
+    private val data = readData()
+    private val signature = AuntSueSignature
+
+    fun processPart1(): Int =
+        data
+            .toMutableSet()
+            .apply { removeIf{!signature.matchesButOutdated(it)} }
+            .single()
+            .id
+
+    fun processPart2(): Int =
+        0
+
+    private fun readData(): Set<AuntSueDescription> =
+        getResourceAsText(filename)
+            .stream()
+            .map(AuntSueDescription::from)
+            .collect(Collectors.toSet())
+
+    private fun getResourceAsText(path: String): List<String> =
+        this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
+
+    private object AuntSueSignature {
+        private val properties: Map<String, Int> = """
+             children: 3
+             cats: 7
+             samoyeds: 2
+             pomeranians: 3
+             akitas: 0
+             vizslas: 0
+             goldfish: 5
+             trees: 3
+             cars: 2
+             perfumes: 1
+             """.trimIndent().lines()
+            .associate { it.substringBefore(": ") to it.substringAfter(": ").toInt() }
+
+        fun matchesButOutdated(description: AuntSueDescription): Boolean =
+            description.properties.all { (key, value) ->
+                properties.containsKey(key) && properties[key] == value
+            }
+    }
+}
