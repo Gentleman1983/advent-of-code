9ced4504215c0b2b49b27cb73ac233b9bae188cc
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-11-25 11:52:42 +0100

    Solved day 11 part 1

--- day11/src/main/java/de/havox_design/aoc2015/day11/MainClass.java
+++ day11/src/main/java/de/havox_design/aoc2015/day11/MainClass.java
@@ -6,7 +6,7 @@ public class MainClass {
     private static final Logger LOGGER = Logger.getLogger(MainClass.class.getName());
 
     public static void main(String[] args) {
-        LOGGER.info("Solution 1: 13");
-        LOGGER.info("Solution 2: 23");
+        LOGGER.info(() -> "Solution 1: " + SantasPasswordPolicy.solvePart1("day11.txt"));
+        LOGGER.info(() -> "Solution 2: " + SantasPasswordPolicy.solvePart2("day11.txt"));
     }
 }
