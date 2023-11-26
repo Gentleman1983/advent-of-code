818c75668cc988319ebae84cfd99d30887d81946
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-11-26 18:49:36 +0100

    Solved day 23 part 1

--- day23/src/main/java/de/havox_design/aoc2015/day23/MainClass.java
+++ day23/src/main/java/de/havox_design/aoc2015/day23/MainClass.java
@@ -6,7 +6,7 @@ public class MainClass {
     private static final Logger LOGGER = Logger.getLogger(MainClass.class.getName());
 
     public static void main(String[] args) {
-        LOGGER.info("Solution 1: 13");
-        LOGGER.info("Solution 2: 23");
+        LOGGER.info(() -> "Solution 1: " + Turing.solvePart1("day23.txt"));
+        LOGGER.info(() -> "Solution 2: " + Turing.solvePart2("day23.txt"));
     }
 }
---
+++ day23/src/main/java/de/havox_design/aoc2015/day23/State.java
@@ -0,0 +1,7 @@
+package de.havox_design.aoc2015.day23;
+
+public class State {
+    int processCount = 0;
+    int registerA = 0;
+    int registerB = 0;
+}
