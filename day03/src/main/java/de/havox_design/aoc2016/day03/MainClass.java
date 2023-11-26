d4a2af0c4768dafbf83d94e540783af2c7ee3b75
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-11-26 21:23:03 +0100

    Added daily processing classes

--- day03/src/main/java/de/havox_design/aoc2016/day03/MainClass.java
+++ day03/src/main/java/de/havox_design/aoc2016/day03/MainClass.java
@@ -4,9 +4,10 @@ import java.util.logging.Logger;
 
 public class MainClass {
     private static final Logger LOGGER = Logger.getLogger(MainClass.class.getName());
+    private static final String FILENAME = "day03.txt";
 
     public static void main(String[] args) {
-        LOGGER.info("Part 1: 13");
-        LOGGER.info("Part 2: 23");
+        LOGGER.info(() -> "Solution for part 1: " + Day03.solvePart1(FILENAME));
+        LOGGER.info(() -> "Solution for part 2: " + Day03.solvePart1(FILENAME));
     }
 }
--- day19/src/main/java/de/havox_design/aoc2016/day19/MainClass.java
+++ day19/src/main/java/de/havox_design/aoc2016/day19/MainClass.java
@@ -4,9 +4,10 @@ import java.util.logging.Logger;
 
 public class MainClass {
     private static final Logger LOGGER = Logger.getLogger(MainClass.class.getName());
+    private static final String FILENAME = "day19.txt";
 
     public static void main(String[] args) {
-        LOGGER.info("Part 1: 13");
-        LOGGER.info("Part 2: 23");
+        LOGGER.info(() -> "Solution for part 1: " + Day19.solvePart1(FILENAME));
+        LOGGER.info(() -> "Solution for part 2: " + Day19.solvePart1(FILENAME));
     }
 }
