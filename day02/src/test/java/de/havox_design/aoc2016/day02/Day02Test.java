be3b409fb7c1ee5b3a66ed80ef7a8218ae404dc3
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-11-26 21:38:46 +0100

    Added sample tests

--- day02/src/test/java/de/havox_design/aoc2016/day02/Day02Test.java
+++ day02/src/test/java/de/havox_design/aoc2016/day02/Day02Test.java
@@ -1,6 +1,12 @@
 package de.havox_design.aoc2016.day02;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class Day02Test {
 
@@ -8,5 +14,29 @@ class Day02Test {
     @Test
     void testMainClass() {
         MainClass.main(new String[0]);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getDataForPart1")
+    void testPart1(String fileName, long expected) {
+        Assertions.assertEquals(expected, Day02.solvePart1(fileName));
+    }
+
+    private static Stream<Arguments> getDataForPart1() {
+        return Stream.of(
+                Arguments.of("part1sample1.txt", 0L)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getDataForPart2")
+    void testPart2(String fileName, long expected) {
+        Assertions.assertEquals(expected, Day02.solvePart2(fileName));
+    }
+
+    private static Stream<Arguments> getDataForPart2() {
+        return Stream.of(
+                Arguments.of("part2sample1.txt", 0L)
+        );
     }
 }
--- day21/src/test/java/de/havox_design/aoc2016/day21/Day21Test.java
+++ day21/src/test/java/de/havox_design/aoc2016/day21/Day21Test.java
@@ -1,6 +1,12 @@
 package de.havox_design.aoc2016.day21;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class Day21Test {
 
@@ -8,5 +14,29 @@ class Day21Test {
     @Test
     void testMainClass() {
         MainClass.main(new String[0]);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getDataForPart1")
+    void testPart1(String fileName, long expected) {
+        Assertions.assertEquals(expected, Day21.solvePart1(fileName));
+    }
+
+    private static Stream<Arguments> getDataForPart1() {
+        return Stream.of(
+                Arguments.of("part1sample1.txt", 0L)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getDataForPart2")
+    void testPart2(String fileName, long expected) {
+        Assertions.assertEquals(expected, Day21.solvePart2(fileName));
+    }
+
+    private static Stream<Arguments> getDataForPart2() {
+        return Stream.of(
+                Arguments.of("part2sample1.txt", 0L)
+        );
     }
 }
