3c390b5a50f5a2cb7fcae0bd4dc989dc37128b0f
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-01 23:08:31 +0100

    Solved day04 part 1

--- day04/src/main/java/de/havox_design/aoc2016/day04/Day04.java
+++ day04/src/main/java/de/havox_design/aoc2016/day04/Day04.java
@@ -3,8 +3,13 @@ package de.havox_design.aoc2016.day04;
 import de.havox_design.aoc2016.utils.DataReader;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class Day04 {
+    private static final Pattern PATTERN = Pattern.compile("(?<name>[a-z-]+)-(?<sectorId>\\d+)\\[(?<checksum>[a-z]+)]");
+
     private final List<String> input;
 
     public Day04(String fileName) {
@@ -22,10 +27,32 @@ public class Day04 {
     }
 
     public long solvePart1() {
+        return parseAll(Room::isValid)
+                .mapToInt(Room::sectorId)
+                .sum();
+    }
+
+    public long solvePart2() {
         return 0L;
     }
 
-    public long solvePart2() {return 0L;
+    private Stream<Room> parseAll(Predicate<Room> predicate) {
+        return input
+                .stream()
+                .map(this::parse)
+                .filter(predicate);
+    }
+
+    private Room parse(String roomName) {
+        var matcher = PATTERN.matcher(roomName);
+        if (matcher.matches()) {
+            return new Room(
+                    matcher.group("name").replace("-", " "),
+                    Integer.parseInt(matcher.group("sectorId")),
+                    matcher.group("checksum")
+            );
+        }
+        throw new IllegalArgumentException("Invalid value " + roomName);
     }
 
     private List<String> readData(String fileName) {
--- day04/src/test/java/de/havox_design/aoc2016/day04/Day04Test.java
+++ day04/src/test/java/de/havox_design/aoc2016/day04/Day04Test.java
@@ -24,7 +24,11 @@ class Day04Test {
 
     private static Stream<Arguments> getDataForPart1() {
         return Stream.of(
-                Arguments.of("part1sample1.txt", 0L)
+                Arguments.of("part1sample1.txt", 123L),
+                Arguments.of("part1sample2.txt", 987L),
+                Arguments.of("part1sample3.txt", 404L),
+                Arguments.of("part1sample4.txt", 0L),
+                Arguments.of("part1sample5.txt", 1514L)
         );
     }
 
