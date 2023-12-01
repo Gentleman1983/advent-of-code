500839bd49bdf26f8885b838d9cbae1847d83543
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-01 23:50:56 +0100

    Solved day05 part 2

--- day05/src/main/java/de/havox_design/aoc2016/day05/Day05.java
+++ day05/src/main/java/de/havox_design/aoc2016/day05/Day05.java
@@ -3,7 +3,10 @@ package de.havox_design.aoc2016.day05;
 import de.havox_design.aoc2016.utils.DataReader;
 import org.apache.commons.codec.digest.DigestUtils;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -25,7 +28,8 @@ public class Day05 {
     }
 
     public String solvePart1() {
-        return IntStream.iterate(0, i -> i + 1)
+        return IntStream
+                .iterate(0, i -> i + 1)
                 .mapToObj(i -> DigestUtils.md5Hex(input + i))
                 .filter(v -> v.startsWith("00000"))
                 .limit(8)
@@ -34,7 +38,21 @@ public class Day05 {
     }
 
     public String solvePart2() {
-        return "";
+        var password = new ArrayList<>(Collections.nCopies(8, " "));
+        var pattern = Pattern.compile("^00000[0-7]");
+
+        for (long i = 0; password.contains(" "); i++) {
+            var md5 = DigestUtils.md5Hex(input + i);
+            if (pattern.matcher(md5).find()) {
+                var index = Integer.parseInt(String.valueOf(md5.charAt(5)));
+
+                if (password.get(index).equals(" ")) {
+                    password.set(index, String.valueOf(md5.charAt(6)));
+                }
+            }
+        }
+
+        return String.join("", password);
     }
 
     private String readData(String fileName) {
