5e88ae9e687f9b5e5fb2c04380c5f3c4dbccb6f5
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-03 21:22:50 +0100

    Added day15 part 1

--- day15/src/test/java/de/havox_design/aoc2016/day15/Day15Test.java
+++ day15/src/test/java/de/havox_design/aoc2016/day15/Day15Test.java
@@ -24,7 +24,7 @@ class Day15Test {
 
     private static Stream<Arguments> getDataForPart1() {
         return Stream.of(
-                Arguments.of("part1sample1.txt", 0L)
+                Arguments.of("part1sample.txt", 0L)
         );
     }
 
