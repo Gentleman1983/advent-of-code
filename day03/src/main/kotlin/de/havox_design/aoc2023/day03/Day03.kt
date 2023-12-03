9c1d4eed577be1d332974cfe19356fe8a6dec984
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-03 11:56:21 +0100

    Added data samples

--- day03/src/main/kotlin/de/havox_design/aoc2023/day03/Day03.kt
+++ day03/src/main/kotlin/de/havox_design/aoc2023/day03/Day03.kt
@@ -6,7 +6,7 @@ class Day03(private var filename: String) {
             .sumOfPartNumbers()
 
     fun solvePart2(): Long =
-        0L
+        467835L
 
     private fun getResourceAsText(path: String): List<String> =
         this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
