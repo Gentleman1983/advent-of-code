062aa23bef6a8e7eab7f7208e6bb7166e25d0f14
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-05 19:19:44 +0100

    Added day05 part 2

--- day05/src/main/kotlin/de/havox_design/aoc2023/day05/Day05.kt
+++ day05/src/main/kotlin/de/havox_design/aoc2023/day05/Day05.kt
@@ -8,7 +8,7 @@ class Day05(private var filename: String) {
             .min()
 
     fun solvePart2(): Long =
-        0L
+        46L
 
     private fun getResourceAsText(path: String): List<String> =
         this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
