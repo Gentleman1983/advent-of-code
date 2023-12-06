a581d782abb618eb0d76fc045f47b799d5a735c4
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-06 18:42:20 +0100

    Solved day06 part 1

--- day06/src/main/kotlin/de/havox_design/aoc2023/day06/Day06.kt
+++ day06/src/main/kotlin/de/havox_design/aoc2023/day06/Day06.kt
@@ -1,11 +1,57 @@
 package de.havox_design.aoc2023.day06
 
 class Day06(private var filename: String) {
-    fun solvePart1(): Long =
-        288L
+    private val DISTANCE_INDEX = 1
+    private val DISTANCE_PREFIX = "Distance: "
+    private val REGEX_SPACE = "\\s+".toRegex()
+    private val TIME_INDEX = 0
+    private val TIME_PREFIX = "Time: "
+
+    fun solvePart1(): Long {
+        val combinations = convertInput(getResourceAsText(filename)).map { game -> simulate(game.first, game.second) }
+        var product = 1L
+
+        for(value in combinations) {
+            product *= value
+        }
+
+        return product
+    }
 
     fun solvePart2(): Long =
         0L
+
+    private fun convertInput(input: List<String>): List<Pair<Long, Long>> {
+        val times = input[TIME_INDEX]
+            .substringAfter(TIME_PREFIX)
+            .trim()
+            .split(REGEX_SPACE)
+            .map { it.toLong() }
+        val distances = input[DISTANCE_INDEX]
+            .substringAfter(DISTANCE_PREFIX)
+            .trim()
+            .split(REGEX_SPACE)
+            .map { it.toLong() }
+        val result = ArrayList<Pair<Long, Long>>()
+
+        for(index in times.indices) {
+            result.add(Pair(times[index], distances[index]))
+        }
+
+        return result
+    }
+
+    private fun simulate(time: Long, goal: Long): Int {
+        var count = 0
+
+        for (i in 0..< time) {
+            if ((time - i) * i > goal) {
+                count++
+            }
+        }
+
+        return count
+    }
 
     private fun getResourceAsText(path: String): List<String> =
         this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
