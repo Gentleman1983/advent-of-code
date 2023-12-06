16107ecbbca85642f5c8eb48f85e8a4628e3e920
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-06 17:55:38 +0100

    Added day06 part 1

--- day06/src/main/kotlin/de/havox_design/aoc2023/day06/Day06.kt
+++ day06/src/main/kotlin/de/havox_design/aoc2023/day06/Day06.kt
@@ -2,7 +2,7 @@ package de.havox_design.aoc2023.day06
 
 class Day06(private var filename: String) {
     fun solvePart1(): Long =
-        0L
+        288L
 
     fun solvePart2(): Long =
         0L
