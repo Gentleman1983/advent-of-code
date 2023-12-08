854eff830de31faed69d0acc67f1a7a0a2c857e3
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-08 21:53:37 +0100

    Added day21 part 1

--- day21/src/test/java/de/havox_design/aoc2016/day21/Day21Test.java
+++ day21/src/test/java/de/havox_design/aoc2016/day21/Day21Test.java
@@ -18,25 +18,25 @@ class Day21Test {
 
     @ParameterizedTest
     @MethodSource("getDataForPart1")
-    void testPart1(String fileName, long expected) {
+    void testPart1(String fileName, String expected, String input) {
         Assertions.assertEquals(expected, Day21.solvePart1(fileName));
     }
 
     private static Stream<Arguments> getDataForPart1() {
         return Stream.of(
-                Arguments.of("part1sample1.txt", 0L)
+                Arguments.of("part1sample.txt", "decab", "abcde")
         );
     }
 
     @ParameterizedTest
     @MethodSource("getDataForPart2")
-    void testPart2(String fileName, long expected) {
+    void testPart2(String fileName, String expected) {
         Assertions.assertEquals(expected, Day21.solvePart2(fileName));
     }
 
     private static Stream<Arguments> getDataForPart2() {
         return Stream.of(
-                Arguments.of("part2sample1.txt", 0L)
+                Arguments.of("part2sample1.txt", "")
         );
     }
 }
