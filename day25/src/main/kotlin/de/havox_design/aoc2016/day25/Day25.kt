597c86d847ebb1fdd993625179d3b66646175c15
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-09 00:47:40 +0100

    Solved day25 part 1

--- day25/src/main/kotlin/de/havox_design/aoc2016/day25/Day25.kt
+++ day25/src/main/kotlin/de/havox_design/aoc2016/day25/Day25.kt
@@ -1,8 +1,17 @@
 package de.havox_design.aoc2016.day25
 
 class Day25(private var filename: String) {
-    fun solvePart1(): Long =
-        0L
+    fun solvePart1(): Int {
+        val target = listOf(1, 2)
+            .map { getResourceAsText(filename)[it].split(" ")[1].toInt() }
+            .let { (a, b) -> a * b }
+
+        return generateSequence(1) { answer ->
+            (answer + 1)
+                .takeUnless { (target + it - 1).toString(2).matches("(10)+".toRegex()) }
+        }
+            .last()
+    }
 
     fun solvePart2(): Long =
         0L
