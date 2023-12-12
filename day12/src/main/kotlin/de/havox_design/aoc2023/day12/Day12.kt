4b525e8c7fb3c06464eb50555b3bc50e4e33cbc4
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-12 19:30:06 +0100

    part 2 but much toooooooooooooooooooooooooo slow...

--- day12/src/main/kotlin/de/havox_design/aoc2023/day12/Day12.kt
+++ day12/src/main/kotlin/de/havox_design/aoc2023/day12/Day12.kt
@@ -14,18 +14,32 @@ class Day12(private var filename: String) {
             }
 
     fun solvePart2(): Long =
-        525152L
+        getResourceAsText(filename)
+            .map { parseInput(it, true) }
+            .sumOf {
+                it
+                    .possibleGroups
+                    .count()
+            }
 
-    private fun parseInput(row: String): Record {
+    private fun parseInput(row: String, folded: Boolean = false): Record {
         val (text, groups) = row
             .split(DATA_DELIMITER)
+        val groupDistribution = groups
+            .split(GROUPS_DELIMITER)
+            .map { it.toInt() }
 
-        return Record(
-            text,
-            groups
-                .split(GROUPS_DELIMITER)
-                .map { it.toInt() }
-        )
+        return when {
+            folded -> {
+                Record(
+                    "$text?$text?$text?$text?$text",
+                    listOf(groupDistribution, groupDistribution, groupDistribution, groupDistribution, groupDistribution)
+                        .flatten()
+                )
+            }
+
+            else -> Record(text, groupDistribution)
+        }
     }
 
     private fun getResourceAsText(path: String): List<String> =
