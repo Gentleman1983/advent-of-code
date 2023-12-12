9a9ac6e08c8554fc8ea2374bdd6ed8f11bfc83d0
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-12 19:03:09 +0100

    Solved day12 part 1

--- day12/src/main/kotlin/de/havox_design/aoc2023/day12/Day12.kt
+++ day12/src/main/kotlin/de/havox_design/aoc2023/day12/Day12.kt
@@ -1,11 +1,32 @@
 package de.havox_design.aoc2023.day12
 
 class Day12(private var filename: String) {
+    private val DATA_DELIMITER = " "
+    private val GROUPS_DELIMITER = ","
+
     fun solvePart1(): Long =
-        21L
+        getResourceAsText(filename)
+            .map { parseInput(it) }
+            .sumOf {
+                it
+                    .possibleGroups
+                    .count()
+            }
 
     fun solvePart2(): Long =
         0L
+
+    private fun parseInput(row: String): Record {
+        val (text, groups) = row
+            .split(DATA_DELIMITER)
+
+        return Record(
+            text,
+            groups
+                .split(GROUPS_DELIMITER)
+                .map { it.toInt() }
+        )
+    }
 
     private fun getResourceAsText(path: String): List<String> =
         this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
