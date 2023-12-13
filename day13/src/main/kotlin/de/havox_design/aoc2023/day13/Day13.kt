5cce81cf5043f04fc6783d4103cb7c922f384c97
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-13 18:58:39 +0100

    Solved day13 part 1

--- day13/src/main/kotlin/de/havox_design/aoc2023/day13/Day13.kt
+++ day13/src/main/kotlin/de/havox_design/aoc2023/day13/Day13.kt
@@ -2,10 +2,23 @@ package de.havox_design.aoc2023.day13
 
 class Day13(private var filename: String) {
     fun solvePart1(): Long =
-        405L
+        makePatterns(getResourceAsText(filename))
+            .sumOf { it.mirrorValue() }
+            .toLong()
 
     fun solvePart2(): Long =
         0L
+
+    private fun makePatterns(input: List<String>): List<Pattern> {
+        return input
+            .withIndex()
+            .filter { (_, line) -> line.isEmpty() }
+            .map { it.index }
+            .let { listOf(-1) + it + (input.lastIndex + 1) }
+            .windowed(2)
+            .map { (before, after) -> input.subList(before + 1, after) }
+            .map(::Pattern)
+    }
 
     private fun getResourceAsText(path: String): List<String> =
         this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
