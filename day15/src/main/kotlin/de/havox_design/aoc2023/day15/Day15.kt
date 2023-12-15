bc21e346a362b0d4ce749d5999765e24b102d197
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-15 14:51:23 +0100

    Solved day15 part 1

--- day15/src/main/kotlin/de/havox_design/aoc2023/day15/Day15.kt
+++ day15/src/main/kotlin/de/havox_design/aoc2023/day15/Day15.kt
@@ -1,11 +1,20 @@
 package de.havox_design.aoc2023.day15
 
 class Day15(private var filename: String) {
+    private val ELEMENT_DELIMITER = ","
+
     fun solvePart1(): Long =
-        1320L
+        getResourceAsText(filename)[0]
+            .split(ELEMENT_DELIMITER)
+            .sumOf { word -> calculateHash(word) }
+            .toLong()
 
     fun solvePart2(): Long =
         0L
+
+    private fun calculateHash(word: String): Int =
+        word
+            .fold(0) { acc, c -> ((acc + c.code) * 17) % 256 }
 
     private fun getResourceAsText(path: String): List<String> =
         this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
