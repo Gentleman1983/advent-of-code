6197fd2383a1e60cf3a5f3b8e4e68a297e0e6471
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-18 11:20:55 +0100

    Solved day18 part 1

--- day18/src/main/kotlin/de/havox_design/aoc2023/day18/Day18.kt
+++ day18/src/main/kotlin/de/havox_design/aoc2023/day18/Day18.kt
@@ -1,11 +1,91 @@
 package de.havox_design.aoc2023.day18
 
 class Day18(private var filename: String) {
+    private val BLACK = "#000000"
+    private val ICON_UP = "U"
+    private val ICON_DOWN = "D"
+    private val ICON_LEFT = "L"
+    private val ICON_RIGHT = "R"
+    private val ELEMENT_DELIMITER = " "
+
     fun solvePart1(): Long =
-        62L
+        mutableMapOf(origin to BLACK)
+            .processInstructions(origin, parseDigInstructions())
+            .calculateTrench()
 
     fun solvePart2(): Long =
         0L
+
+    private fun MutableMap<Coordinate, String>.processInstructions(
+        start: Coordinate,
+        instructions: List<DigInstruction>
+    ): MutableMap<Coordinate, String> {
+        var current = start
+
+        instructions
+            .forEach { instruction ->
+                repeat(instruction.amount) {
+                    current = instruction.direction + current
+                    set(current, instruction.colorCode)
+                }
+            }
+
+        return this
+    }
+
+    private fun MutableMap<Coordinate, String>.calculateTrench(): Long {
+        val xRange = xRange()
+            .let { (a, b) ->
+                a..b
+            }
+        val yRange = yRange()
+            .let { (a, b) ->
+                a..b
+            }
+        val connectNorth = keys
+            .filter { FourDirectionFlipped.UP + it in this }
+            .toSet()
+        var count = 0L
+
+        yRange.forEach { y ->
+            var border = 0
+
+            xRange.forEach { x ->
+                val coordinate = Coordinate(x, y)
+
+                border += when {
+                    coordinate in connectNorth -> 1
+                    else -> 0
+                }
+                when {
+                    containsKey(coordinate) -> count++
+                    border % 2 == 1 -> count++
+                }
+            }
+        }
+
+        return count
+    }
+
+    private fun parseDigInstructions() =
+        getResourceAsText(filename)
+            .map { row ->
+                row
+                    .split(ELEMENT_DELIMITER, limit = 3)
+                    .let { (direction, steps, color) ->
+                        DigInstruction(
+                            when (direction) {
+                                ICON_RIGHT -> FourDirectionFlipped.RIGHT
+                                ICON_LEFT -> FourDirectionFlipped.LEFT
+                                ICON_UP -> FourDirectionFlipped.UP
+                                ICON_DOWN -> FourDirectionFlipped.DOWN
+                                else -> error("Invalid input : $direction $steps $color")
+                            },
+                            steps.toInt(),
+                            color.drop(1).dropLast(1)
+                        )
+                    }
+            }
 
     private fun getResourceAsText(path: String): List<String> =
         this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
---
+++ day18/src/main/kotlin/de/havox_design/aoc2023/day18/DigInstruction.kt
@@ -0,0 +1,15 @@
+package de.havox_design.aoc2023.day18
+
+data class DigInstruction(val direction: FourDirectionFlipped, val amount: Int, val colorCode: String) {
+    fun transformInstruction(): DigInstruction {
+        val steps = colorCode.drop(1).take(5).toInt(16)
+        val direction = when (colorCode.last()) {
+            '0' -> FourDirectionFlipped.RIGHT
+            '1' -> FourDirectionFlipped.DOWN
+            '2' -> FourDirectionFlipped.LEFT
+            '3' -> FourDirectionFlipped.UP
+            else -> error("invalid $colorCode")
+        }
+        return copy(direction = direction, amount = steps)
+    }
+}
