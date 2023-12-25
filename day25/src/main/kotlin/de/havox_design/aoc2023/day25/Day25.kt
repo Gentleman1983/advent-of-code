afdf257e97aaa522a4e183a4a43b8d56768e6a99
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-25 11:41:06 +0100

    Solved day25 part 1

--- day25/src/main/kotlin/de/havox_design/aoc2023/day25/Day25.kt
+++ day25/src/main/kotlin/de/havox_design/aoc2023/day25/Day25.kt
@@ -1,11 +1,48 @@
 package de.havox_design.aoc2023.day25
 
+import org.jgrapht.Graph
+import org.jgrapht.alg.flow.GusfieldGomoryHuCutTree
+import org.jgrapht.graph.DefaultEdge
+import org.jgrapht.graph.SimpleGraph
+
 class Day25(private var filename: String) {
+    private val CONNECTING_DELIMITER = ": "
+    private val ELEMENT_DELIMITER = " "
+
     fun solvePart1(): Long =
-        54L
+        splitGroups(getResourceAsText(filename))
+            .toLong()
 
     fun solvePart2(): Long =
         0L
+
+    private fun splitGroups(input: List<String>): Int {
+        val graph: Graph<String, DefaultEdge> = SimpleGraph(DefaultEdge::class.java)
+
+        for (row in input) {
+            val split = row
+                .split(CONNECTING_DELIMITER)
+            val source = split[0]
+            val destinationSplit = split[1]
+                .split(ELEMENT_DELIMITER)
+                .toMutableList()
+
+            graph.addVertex(source)
+            for (destination in destinationSplit) {
+                graph.addVertex(destination)
+                graph.addEdge(source, destination)
+            }
+        }
+
+        val gusfieldGomoryHuCutTree = GusfieldGomoryHuCutTree(graph)
+
+        gusfieldGomoryHuCutTree.calculateMinCut()
+
+        val sourcePartition = gusfieldGomoryHuCutTree.sourcePartition
+        val remainingPartitionSize = graph.vertexSet().size - sourcePartition.size
+
+        return remainingPartitionSize * sourcePartition.size
+    }
 
     private fun getResourceAsText(path: String): List<String> =
         this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
