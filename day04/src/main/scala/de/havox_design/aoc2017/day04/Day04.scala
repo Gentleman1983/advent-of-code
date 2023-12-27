b1af9de9e885bafbd87608e7232bb713801e7dfb
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-27 15:24:04 +0100

    Solved day04 part 1

--- day04/src/main/scala/de/havox_design/aoc2017/day04/Day04.scala
+++ day04/src/main/scala/de/havox_design/aoc2017/day04/Day04.scala
@@ -2,11 +2,15 @@ package de.havox_design.aoc2017.day04
 
 object Day04 {
   def solvePart1(filename: String): Int =
-    0
+    readData(filename)
+      .count{ row =>
+        val rowElements = row.split(" ")
+        rowElements.distinct.length == rowElements.length
+      }
 
   def solvePart2(filename: String): Int =
     0
 
-  def readData(filename: String): String =
-    scala.io.Source.fromResource(filename).mkString
+  def readData(filename: String): Iterator[String] =
+    scala.io.Source.fromResource(filename).getLines()
 }
