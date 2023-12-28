7f1b36bc00e80a44b434ec778c90806ca1e57807
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-28 13:45:20 +0100

    Solved day10 part 2

--- day10/src/main/scala/de/havox_design/aoc2017/day10/Day10.scala
+++ day10/src/main/scala/de/havox_design/aoc2017/day10/Day10.scala
@@ -4,8 +4,16 @@ object Day10 {
   def solvePart1(filename: String, elements: Int = 256): Int =
     calculateProductOfFirstTwoElements(readData(filename).next(), elements)
 
-  def solvePart2(filename: String): String =
-    ""
+  def solvePart2(filename: String): String = {
+    val input = if (!readData(filename).hasNext) then {
+      ""
+    }
+    else {
+      readData(filename).next()
+    }
+
+    calculateKnotHash(input)
+  }
 
   def main(args: Array[String]): Unit = {
     def dayFileName = "day07.txt"
@@ -14,9 +22,11 @@ object Day10 {
     println("Solution for part2: " + solvePart2(dayFileName))
   }
 
-  private def process(initialKnotState: KnotState, lengths: Seq[Int]): KnotState =
-    lengths
-      .foldLeft(initialKnotState)(_.reversed(_))
+  private def process(initialKnotState: KnotState, lengths: Seq[Int], rounds: Int = 1): KnotState =
+    (0 until rounds).foldLeft(initialKnotState) { (knotState, round) =>
+      lengths
+        .foldLeft(knotState)(_.reversed(_))
+    }
 
   private def calculateProductOfFirstTwoElements(input: String, elements: Int = 256): Int = {
     val lengths = input
@@ -32,6 +42,23 @@ object Day10 {
     process(KnotState(Vector.range(0, elements)), lengths)
       .checkProduct
   }
+
+  private def calculateKnotHash(input: String): String =
+    convertToHexString(transformSparseHashesToDenseHash(process(KnotState(), asciiLengths(input), 64).elements))
+
+  private def transformSparseHashesToDenseHash(sparse: Seq[Int]): Seq[Int] =
+    sparse
+      .grouped(16)
+      .map(_.reduce(_ ^ _))
+      .toSeq
+
+  private def convertToHexString(dense: Seq[Int]): String =
+    dense
+      .map("%02x".format(_))
+      .mkString
+
+  private def asciiLengths(input: String): Seq[Int] =
+    input.map(_.toInt) ++ Seq(17, 31, 73, 47, 23)
 
   private def readData(filename: String): Iterator[String] =
     scala
