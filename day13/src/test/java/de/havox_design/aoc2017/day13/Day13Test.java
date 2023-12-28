8961c886bae92cbf9e73fa57563a520aa05cf3eb
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-28 14:47:03 +0100

    Added day13 part 1

--- day13/src/test/java/de/havox_design/aoc2017/day13/Day13Test.java
+++ day13/src/test/java/de/havox_design/aoc2017/day13/Day13Test.java
@@ -24,7 +24,7 @@ class Day13Test {
 
     private static Stream<Arguments> getDataForPart1() {
         return Stream.of(
-                Arguments.of("part1sample1.txt", 0L)
+                Arguments.of("part1sample.txt", 24L)
         );
     }
 
