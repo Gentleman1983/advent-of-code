67cf89d98e3d9a05a924ba001ce10f66f51945d1
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-28 12:11:41 +0100

    Solved day09 part 1

--- day09/src/main/scala/de/havox_design/aoc2017/day09/Day09.scala
+++ day09/src/main/scala/de/havox_design/aoc2017/day09/Day09.scala
@@ -2,7 +2,8 @@ package de.havox_design.aoc2017.day09
 
 object Day09 {
   def solvePart1(filename: String): Int =
-    0
+    calculate(readData(filename).next().trim)
+      .groupScore
 
   def solvePart2(filename: String): Int =
     0
@@ -13,6 +14,13 @@ object Day09 {
     println("Solution for part1: " + solvePart1(dayFileName))
     println("Solution for part2: " + solvePart2(dayFileName))
   }
+
+  private def calculate(input: String): State =
+    val initial = State(1, 0, false, 0, false)
+
+    input
+      .foldLeft(initial)((state, next) => state
+        .step(next))
 
   private def readData(filename: String): Iterator[String] =
     scala
