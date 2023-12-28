67511cb367075e2343261ddf7190617d5b6aecef
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-28 13:18:14 +0100

    Solved day10 part 1

--- day10/src/main/scala/de/havox_design/aoc2017/day10/Day10.scala
+++ day10/src/main/scala/de/havox_design/aoc2017/day10/Day10.scala
@@ -1,8 +1,8 @@
 package de.havox_design.aoc2017.day10
 
 object Day10 {
-  def solvePart1(filename: String): Int =
-    12
+  def solvePart1(filename: String, elements: Int = 256): Int =
+    calculateProductOfFirstTwoElements(readData(filename).next(), elements)
 
   def solvePart2(filename: String): Int =
     0
@@ -12,6 +12,25 @@ object Day10 {
 
     println("Solution for part1: " + solvePart1(dayFileName))
     println("Solution for part2: " + solvePart2(dayFileName))
+  }
+
+  private def process(initialKnotState: KnotState, lengths: Seq[Int]): KnotState =
+    lengths
+      .foldLeft(initialKnotState)(_.reversed(_))
+
+  private def calculateProductOfFirstTwoElements(input: String, elements: Int = 256): Int = {
+    val lengths = input
+      .trim
+      .split(",")
+      .toSeq
+      .map(
+        _
+          .trim
+          .toInt
+      )
+
+    process(KnotState(Vector.range(0, elements)), lengths)
+      .checkProduct
   }
 
   private def readData(filename: String): Iterator[String] =
--- day10/src/test/scala/de/havox_design/aoc2017/day10/Day10Test.scala
+++ day10/src/test/scala/de/havox_design/aoc2017/day10/Day10Test.scala
@@ -11,7 +11,7 @@ class Day10Test extends AnyFunSuite {
   }
 
   test("Day 10 - Part 1 - Sample") {
-    assert(solvePart1("part1sample.txt") == 12)
+    assert(solvePart1("part1sample.txt", 5) == 12)
   }
 
   test("Day 10 - Part 2") {
