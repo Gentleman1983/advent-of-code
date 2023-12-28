29bc11d939863fd36851f30a2ce08154d6ac1947
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-28 11:48:31 +0100

    Solved day08 part 1

--- day08/src/main/scala/de/havox_design/aoc2017/day08/Day08.scala
+++ day08/src/main/scala/de/havox_design/aoc2017/day08/Day08.scala
@@ -2,7 +2,11 @@ package de.havox_design.aoc2017.day08
 
 object Day08 {
   def solvePart1(filename: String): Int =
-    1
+    compute(readData(filename)
+      .toIndexedSeq)
+      ._1
+      .values
+      .max
 
   def solvePart2(filename: String): Int =
     0
@@ -13,6 +17,44 @@ object Day08 {
     println("Solution for part1: " + solvePart1(dayFileName))
     println("Solution for part2: " + solvePart2(dayFileName))
   }
+
+  def compute(input: Seq[String]): (Map[String, Int], Int) =
+    val initial = Map
+      .empty[String, Int]
+      .withDefaultValue(0) -> 0
+
+    input
+      .map(_.split(" "))
+      .foldLeft(initial) { case ((registers, max), instruction) =>
+        val Array(destination, operation, amount, _, test, condition, threshold) = instruction
+        val left = registers(test)
+        val right = threshold
+          .toInt
+        val valid = condition match
+          case ">" => left > right
+          case "<" => left < right
+          case ">=" => left >= right
+          case "<=" => left <= right
+          case "==" => left == right
+          case "!=" => left != right
+
+        val modifier = if !valid then {
+          0
+        }
+        else {
+          if operation != "inc" then {
+            -1
+          }
+          else {
+            1
+          }
+        }
+
+        val next = registers(destination) + amount.toInt * modifier
+        registers
+          .updated(destination, next) -> max
+          .max(next)
+      }
 
   private def readData(filename: String): Iterator[String] =
     scala
