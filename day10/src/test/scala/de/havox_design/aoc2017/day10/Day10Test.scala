82a2b9bcdb2ab23545e2c34af2bac32327228486
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-28 03:33:15 +0100

    Switched day 7 to 10 stubs to Scala

---
+++ day10/src/test/scala/de/havox_design/aoc2017/day10/Day10Test.scala
@@ -0,0 +1,24 @@
+package de.havox_design.aoc2017.day10
+
+import Day10._
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day10Test extends AnyFunSuite {
+  def dayFileName = "day10.txt"
+
+  test("Day 10 - Part 1") {
+    println("Solution for part1: " + solvePart1(dayFileName))
+  }
+
+  test("Day 10 - Part 1 - Sample") {
+    assert(solvePart1("sample.txt") == 0)
+  }
+
+  test("Day 10 - Part 2") {
+    println("Solution for part2: " + solvePart2(dayFileName))
+  }
+
+  test("Day 10 - Part 2 - Sample") {
+    assert(solvePart2("sample.txt") == 0)
+  }
+}
