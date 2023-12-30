23eb600ecb9853c47ccda42c260fe73f68eca5b8
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-30 11:39:44 +0100

    Fixed sonar issue

--- day21/src/main/java/de/havox_design/aoc2017/day21/Day21.java
+++ day21/src/main/java/de/havox_design/aoc2017/day21/Day21.java
@@ -5,7 +5,6 @@ import de.havox_design.aoc.utils.DataReader;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Day21 {
