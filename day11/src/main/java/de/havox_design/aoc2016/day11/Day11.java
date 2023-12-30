736a2d4dc4b644ab99e943ac5090fadea73864f0
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-30 16:02:00 +0100

    Update README

---
+++ day11/src/main/java/de/havox_design/aoc2016/day11/Day11.java
@@ -0,0 +1,34 @@
+package de.havox_design.aoc2016.day11;
+
+import de.havox_design.aoc2016.utils.DataReader;
+
+import java.util.List;
+
+public class Day11 {
+    private final List<String> input;
+
+    public Day11(String fileName) {
+        input = readData(fileName);
+    }
+
+    public static long solvePart1(String fileName) {
+        Day11 instance = new Day11(fileName);
+        return instance.solvePart1();
+    }
+
+    public static long solvePart2(String fileName) {
+        Day11 instance = new Day11(fileName);
+        return instance.solvePart2();
+    }
+
+    public long solvePart1() {
+        return 0L;
+    }
+
+    public long solvePart2() {return 0L;
+    }
+
+    private List<String> readData(String fileName) {
+        return DataReader.readData(fileName, MainClass.class);
+    }
+}
---
+++ day19/src/main/java/de/havox_design/aoc2016/day19/MainClass.java
@@ -0,0 +1,13 @@
+package de.havox_design.aoc2016.day19;
+
+import java.util.logging.Logger;
+
+public class MainClass {
+    private static final Logger LOGGER = Logger.getLogger(MainClass.class.getName());
+    private static final String FILENAME = "day19.txt";
+
+    public static void main(String[] args) {
+        LOGGER.info(() -> "Solution for part 1: " + Day19.solvePart1(FILENAME));
+        LOGGER.info(() -> "Solution for part 2: " + Day19.solvePart1(FILENAME));
+    }
+}
