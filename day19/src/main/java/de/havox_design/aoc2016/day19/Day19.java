50f626064b9a76abcaa6e371670603797591c384
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-30 15:56:50 +0100

    Update README

---
+++ day19/src/main/java/de/havox_design/aoc2016/day19/Day19.java
@@ -0,0 +1,34 @@
+package de.havox_design.aoc2016.day19;
+
+import de.havox_design.aoc2016.utils.DataReader;
+
+import java.util.List;
+
+public class Day19 {
+    private final List<String> input;
+
+    public Day19(String fileName) {
+        input = readData(fileName);
+    }
+
+    public static long solvePart1(String fileName) {
+        Day19 instance = new Day19(fileName);
+        return instance.solvePart1();
+    }
+
+    public static long solvePart2(String fileName) {
+        Day19 instance = new Day19(fileName);
+        return instance.solvePart2();
+    }
+
+    public long solvePart1() {
+        return 0L;
+    }
+
+    public long solvePart2() {return 0L;
+    }
+
+    private List<String> readData(String fileName) {
+        return DataReader.readData(fileName, MainClass.class);
+    }
+}
