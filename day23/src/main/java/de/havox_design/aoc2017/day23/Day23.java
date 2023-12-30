bacf7165a4197b34e45d667d9a2436b91f6cf75c
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2023-12-30 14:35:05 +0100

    Solved day23 part 2

--- day23/src/main/java/de/havox_design/aoc2017/day23/Day23.java
+++ day23/src/main/java/de/havox_design/aoc2017/day23/Day23.java
@@ -5,15 +5,20 @@ import de.havox_design.aoc2017.day18.Day18;
 import de.havox_design.aoc2017.day18.State;
 import de.havox_design.aoc2017.day18.instructions.Instruction;
 
+import java.math.BigInteger;
+import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class Day23 {
     private final List<String> input;
-    private final List<Instruction> program;
+    private final List<Instruction> program1;
 
     public Day23(String fileName) {
         input = readData(fileName);
-        program = new Day18(fileName).parseProgram(input);
+        program1 = new Day18(fileName).parseProgram(input);
     }
 
     public static long solvePart1(String fileName) {
@@ -27,6 +32,21 @@ public class Day23 {
     }
 
     public long solvePart1() {
+        return processProgram(program1).getMultiplicationInstructionCount();
+    }
+
+    public long solvePart2() {
+        long counter = 0L;
+
+        for(int i = 109300; i <= 126300; i += 17) {
+            if (!BigInteger.valueOf(i).isProbablePrime(10)) {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+    private State processProgram(List<Instruction> program) {
         final State state = new State(0);
 
         while (state.getPosition() < program.size()) {
@@ -37,10 +57,7 @@ public class Day23 {
 
             state.incrementPosition();
         }
-        return state.getMultiplicationInstructionCount();
-    }
-
-    public long solvePart2() {return 0L;
+        return state;
     }
 
     private List<String> readData(String fileName) {
