7bc4a982ad4571645385f1d335110f8d4f5b382b
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-01-24 23:26:30 +0100

    #27: moved some duplications on Main Classes to interface

--- aoc2016/src/main/kotlin/de/havox_design/aoc2016/MainClass.kt
+++ aoc2016/src/main/kotlin/de/havox_design/aoc2016/MainClass.kt
@@ -1,5 +1,6 @@
 package de.havox_design.aoc2016
 
+import de.havox_design.aoc.utils.kotlin.AocMainClassHelper
 import de.havox_design.aoc2016.day01.NoTimeForATaxicab
 import de.havox_design.aoc2016.day02.BathroomSecurity
 import de.havox_design.aoc2016.day03.SquaresWithThreeSides
@@ -26,326 +27,296 @@ import de.havox_design.aoc2016.day23.SafeCracking
 import de.havox_design.aoc2016.day24.AirDuctSpelunking
 import de.havox_design.aoc2016.day25.ClockSignal
 
-class MainClass {
+class MainClass: AocMainClassHelper {
+    override fun getYear(): Int = 2016
+
+    override fun processYear(args: Array<String>) {
+        val daysSelected = args
+            .any { argument -> argument.startsWith("day") }
+
+        var day = 1
+        day(
+            getDayString(day),
+            NoTimeForATaxicab(getFileName(day))::solvePart1,
+            NoTimeForATaxicab(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 2
+        day(
+            getDayString(day),
+            BathroomSecurity(getFileName(day))::solvePart1,
+            BathroomSecurity(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 3
+        day(
+            getDayString(day),
+            SquaresWithThreeSides(getFileName(day))::solvePart1,
+            SquaresWithThreeSides(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 4
+        day(
+            getDayString(day),
+            SecurityThroughObscurity(getFileName(day))::solvePart1,
+            SecurityThroughObscurity(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 5
+        day(
+            getDayString(day),
+            HowAboutANiceGameOfChess(getFileName(day))::solvePart1,
+            HowAboutANiceGameOfChess(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 6
+        day(
+            getDayString(day),
+            SignalsAndNoise(getFileName(day))::solvePart1,
+            SignalsAndNoise(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 7
+        day(
+            getDayString(day),
+            InternetProtocolVersion7(getFileName(day))::solvePart1,
+            InternetProtocolVersion7(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 8
+        day(
+            getDayString(day),
+            TwoFactorAuthentication(getFileName(day))::solvePart1,
+            TwoFactorAuthentication(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 9
+        day(
+            getDayString(day),
+            ExplosivesInCyberspace(getFileName(day))::solvePart1,
+            ExplosivesInCyberspace(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 10
+        day(
+            getDayString(day),
+            ::balanceBotsPart1,
+            BalanceBots(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 11
+        day(
+            getDayString(day),
+            RadioisotopeThermoelectricGenerators(getFileName(day))::solvePart1,
+            RadioisotopeThermoelectricGenerators(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 12
+        if (!args.contains("testing")) {
+            day(
+                getDayString(day),
+                LeonardosMonorail(getFileName(day))::solvePart1,
+                LeonardosMonorail(getFileName(day))::solvePart2,
+                daysSelected,
+                args
+            )
+        }
+
+        day = 13
+        day(
+            getDayString(day),
+            ::aMazeOfTwistyLittleCubiclesPart1,
+            ::aMazeOfTwistyLittleCubiclesPart2,
+            daysSelected,
+            args
+        )
+
+        day = 14
+        day(
+            getDayString(day),
+            OneTimePad(getFileName(day))::solvePart1,
+            OneTimePad(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 15
+        day(
+            getDayString(day),
+            TimingIsEverything(getFileName(day))::solvePart1,
+            TimingIsEverything(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 16
+        day(
+            getDayString(day),
+            ::dragonChecksumPart1,
+            ::dragonChecksumPart2,
+            daysSelected,
+            args
+        )
+
+        day = 17
+        day(
+            getDayString(day),
+            TwoStepsForward(getFileName(day))::solvePart1,
+            TwoStepsForward(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 18
+        day(
+            getDayString(day),
+            ::likeARoguePart1,
+            LikeARogue(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 19
+        day(
+            getDayString(day),
+            AnElephantNamedJoseph(getFileName(day))::solvePart1,
+            AnElephantNamedJoseph(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 20
+        day(
+            getDayString(day),
+            FirewallRules(getFileName(day))::solvePart1,
+            FirewallRules(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 21
+        day(
+            getDayString(day),
+            ::scrambledLettersAndHashPart1,
+            ::scrambledLettersAndHashPart2,
+            daysSelected,
+            args
+        )
+
+        day = 22
+        day(
+            getDayString(day),
+            GridComputing(getFileName(day))::solvePart1,
+            GridComputing(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 23
+        if (!args.contains("testing")) {
+            day(
+                getDayString(day),
+                SafeCracking(getFileName(day))::solvePart1,
+                SafeCracking(getFileName(day))::solvePart2,
+                daysSelected,
+                args
+            )
+        }
+
+        day = 24
+        day(
+            getDayString(day),
+            AirDuctSpelunking(getFileName(day))::solvePart1,
+            AirDuctSpelunking(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+
+        day = 25
+        day(
+            getDayString(day),
+            ClockSignal(getFileName(day))::solvePart1,
+            ClockSignal(getFileName(day))::solvePart2,
+            daysSelected,
+            args
+        )
+    }
+
+    private fun balanceBotsPart1(): String {
+        val filename = getFileName(10)
+
+        return BalanceBots.solvePart1(filename).toString()
+    }
+
+    private fun aMazeOfTwistyLittleCubiclesPart1(): String {
+        val filename = getFileName(13)
+
+        return AMazeOfTwistyLittleCubicles.solvePart1(filename).toString()
+    }
+
+    private fun aMazeOfTwistyLittleCubiclesPart2(): String {
+        val filename = getFileName(13)
+
+        return AMazeOfTwistyLittleCubicles.solvePart2(filename).toString()
+    }
+
+    private fun dragonChecksumPart1(): String {
+        val filename = getFileName(16)
+
+        return DragonChecksum.solvePart1(filename).toString()
+    }
+
+    private fun dragonChecksumPart2(): String {
+        val filename = getFileName(16)
+
+        return DragonChecksum.solvePart2(filename).toString()
+    }
+
+    private fun likeARoguePart1(): String {
+        val filename = getFileName(18)
+
+        return LikeARogue.solvePart1(filename).toString()
+    }
+
+    private fun scrambledLettersAndHashPart1(): String {
+        val filename = getFileName(21)
+
+        return ScrambledLettersAndHash.solvePart1(filename).toString()
+    }
+
+    private fun scrambledLettersAndHashPart2(): String {
+        val filename = getFileName(21)
+
+        return ScrambledLettersAndHash.solvePart2(filename).toString()
+    }
+
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val daysSelected = args
-                .any { argument -> argument.startsWith("day") }
-
-            var day = 1
-            day(
-                getDayString(day),
-                NoTimeForATaxicab(getFileName(day))::solvePart1,
-                NoTimeForATaxicab(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 2
-            day(
-                getDayString(day),
-                BathroomSecurity(getFileName(day))::solvePart1,
-                BathroomSecurity(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 3
-            day(
-                getDayString(day),
-                SquaresWithThreeSides(getFileName(day))::solvePart1,
-                SquaresWithThreeSides(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 4
-            day(
-                getDayString(day),
-                SecurityThroughObscurity(getFileName(day))::solvePart1,
-                SecurityThroughObscurity(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 5
-            day(
-                getDayString(day),
-                HowAboutANiceGameOfChess(getFileName(day))::solvePart1,
-                HowAboutANiceGameOfChess(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 6
-            day(
-                getDayString(day),
-                SignalsAndNoise(getFileName(day))::solvePart1,
-                SignalsAndNoise(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 7
-            day(
-                getDayString(day),
-                InternetProtocolVersion7(getFileName(day))::solvePart1,
-                InternetProtocolVersion7(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 8
-            day(
-                getDayString(day),
-                TwoFactorAuthentication(getFileName(day))::solvePart1,
-                TwoFactorAuthentication(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 9
-            day(
-                getDayString(day),
-                ExplosivesInCyberspace(getFileName(day))::solvePart1,
-                ExplosivesInCyberspace(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 10
-            day(
-                getDayString(day),
-                ::balanceBotsPart1,
-                BalanceBots(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 11
-            day(
-                getDayString(day),
-                RadioisotopeThermoelectricGenerators(getFileName(day))::solvePart1,
-                RadioisotopeThermoelectricGenerators(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 12
-            if (!args.contains("testing")) {
-                day(
-                    getDayString(day),
-                    LeonardosMonorail(getFileName(day))::solvePart1,
-                    LeonardosMonorail(getFileName(day))::solvePart2,
-                    daysSelected,
-                    args
-                )
-            }
-
-            day = 13
-            day(
-                getDayString(day),
-                ::aMazeOfTwistyLittleCubiclesPart1,
-                ::aMazeOfTwistyLittleCubiclesPart2,
-                daysSelected,
-                args
-            )
-
-            day = 14
-            day(
-                getDayString(day),
-                OneTimePad(getFileName(day))::solvePart1,
-                OneTimePad(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 15
-            day(
-                getDayString(day),
-                TimingIsEverything(getFileName(day))::solvePart1,
-                TimingIsEverything(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 16
-            day(
-                getDayString(day),
-                ::dragonChecksumPart1,
-                ::dragonChecksumPart2,
-                daysSelected,
-                args
-            )
-
-            day = 17
-            day(
-                getDayString(day),
-                TwoStepsForward(getFileName(day))::solvePart1,
-                TwoStepsForward(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 18
-            day(
-                getDayString(day),
-                ::likeARoguePart1,
-                LikeARogue(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 19
-            day(
-                getDayString(day),
-                AnElephantNamedJoseph(getFileName(day))::solvePart1,
-                AnElephantNamedJoseph(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 20
-            day(
-                getDayString(day),
-                FirewallRules(getFileName(day))::solvePart1,
-                FirewallRules(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 21
-            day(
-                getDayString(day),
-                ::scrambledLettersAndHashPart1,
-                ::scrambledLettersAndHashPart2,
-                daysSelected,
-                args
-            )
-
-            day = 22
-            day(
-                getDayString(day),
-                GridComputing(getFileName(day))::solvePart1,
-                GridComputing(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 23
-            if (!args.contains("testing")) {
-                day(
-                    getDayString(day),
-                    SafeCracking(getFileName(day))::solvePart1,
-                    SafeCracking(getFileName(day))::solvePart2,
-                    daysSelected,
-                    args
-                )
-            }
-
-            day = 24
-            day(
-                getDayString(day),
-                AirDuctSpelunking(getFileName(day))::solvePart1,
-                AirDuctSpelunking(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-
-            day = 25
-            day(
-                getDayString(day),
-                ClockSignal(getFileName(day))::solvePart1,
-                ClockSignal(getFileName(day))::solvePart2,
-                daysSelected,
-                args
-            )
-        }
-
-        private fun balanceBotsPart1(): String {
-            val filename = getFileName(10)
-
-            return BalanceBots.solvePart1(filename).toString()
-        }
-
-        private fun aMazeOfTwistyLittleCubiclesPart1(): String {
-            val filename = getFileName(13)
-
-            return AMazeOfTwistyLittleCubicles.solvePart1(filename).toString()
-        }
-
-        private fun aMazeOfTwistyLittleCubiclesPart2(): String {
-            val filename = getFileName(13)
-
-            return AMazeOfTwistyLittleCubicles.solvePart2(filename).toString()
-        }
-
-        private fun dragonChecksumPart1(): String {
-            val filename = getFileName(16)
-
-            return DragonChecksum.solvePart1(filename).toString()
-        }
-
-        private fun dragonChecksumPart2(): String {
-            val filename = getFileName(16)
-
-            return DragonChecksum.solvePart2(filename).toString()
-        }
-
-        private fun likeARoguePart1(): String {
-            val filename = getFileName(18)
-
-            return LikeARogue.solvePart1(filename).toString()
-        }
-
-        private fun scrambledLettersAndHashPart1(): String {
-            val filename = getFileName(21)
-
-            return ScrambledLettersAndHash.solvePart1(filename).toString()
-        }
-
-        private fun scrambledLettersAndHashPart2(): String {
-            val filename = getFileName(21)
-
-            return ScrambledLettersAndHash.solvePart2(filename).toString()
-        }
-
-        private fun day(
-            day: String,
-            solutionPart1: () -> Any,
-            solutionPart2: () -> Any,
-            daysSelected: Boolean,
-            args: Array<String>
-        ) {
-            if (!daysSelected || args.contains(day.lowercase())) {
-                solveDay(day, solutionPart1.invoke(), solutionPart2.invoke())
-            }
-        }
-
-        private fun solveDay(day: String, solutionPart1: Any, solutionPart2: Any) {
-            println("${day}: Solution part 1: $solutionPart1")
-            println("${day}: Solution part 2: $solutionPart2")
-        }
-
-        private fun getFileName(day: Int): String {
-            val year = 2016
-            var dayString = day.toString()
-
-            if (day < 10) {
-                dayString = "0$dayString"
-            }
-
-            return "de/havox_design/aoc${year}/day${dayString}/day${dayString}.txt"
-        }
-
-        private fun getDayString(day: Int): String {
-            var dayString = day.toString()
-            if (day < 10) {
-                dayString = "0$dayString"
-            }
-
-            return "Day${dayString}"
+            val instance = MainClass()
+            instance.processYear(args)
         }
     }
 }
