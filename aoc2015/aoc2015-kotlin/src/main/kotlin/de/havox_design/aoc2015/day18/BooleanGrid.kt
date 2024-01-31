d24946012006cca10204c1022ff9264425407da9
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-01-31 12:46:25 +0100

    #28: Optimized AoC2015 days 18

--- aoc2015/aoc2015-kotlin/src/main/kotlin/de/havox_design/aoc2015/day18/BooleanGrid.kt
+++ aoc2015/aoc2015-kotlin/src/main/kotlin/de/havox_design/aoc2015/day18/BooleanGrid.kt
@@ -1,59 +1,88 @@
 package de.havox_design.aoc2015.day18
 
-typealias Point = Pair<Int, Int>
+import de.havox_design.aoc.utils.kotlin.model.positions.Position2d
 
 class BooleanGrid(val size: Int) : Grid {
-    private val lights = mutableSetOf<Point>()
+    private val lights = mutableSetOf<Position2d<Int>>()
 
-    override fun turnOn(x: Int, y: Int) {
-        lights += Point(x, y)
+    override fun turnOn(position: Position2d<Int>) {
+        lights += Position2d(position.x, position.y)
     }
 
-    override fun turnOff(x: Int, y: Int) {
-        lights -= Point(x, y)
+    override fun turnOff(position: Position2d<Int>) {
+        lights -= Position2d(position.x, position.y)
     }
 
-    override fun toggle(x: Int, y: Int) =
-        Point(x, y).let { if (it in lights) lights -= it else lights += it }
+    override fun toggle(position: Position2d<Int>) =
+        position
+            .let {
+                if (it in lights) {
+                    lights -= it
+                } else {
+                    lights += it
+                }
+            }
 
-    override fun lights() = lights.size
+    override fun lights() =
+        lights.size
 
-    fun on(x: Int, y: Int) = Point(x, y) in lights
+    private fun on(x: Int, y: Int) =
+        Position2d(x, y) in lights
 
     fun next(): BooleanGrid {
         val new = BooleanGrid(size)
-        for (x in 0 until size) for (y in 0 until size)
-            if (on(x, y))
-                when (neighbors(x, y)) {
-                    2, 3 -> new.turnOn(x, y)
-                }
-            else
-                when (neighbors(x, y)) {
-                    3 -> new.turnOn(x, y)
-                }
+
+        for (x in 0 until size) {
+            for (y in 0 until size) {
+                val currentPosition = Position2d(x, y)
+
+                if (on(x, y))
+                    when (neighbors(currentPosition)) {
+                        2, 3 -> new.turnOn(currentPosition)
+                    }
+                else
+                    when (neighbors(currentPosition)) {
+                        3 -> new.turnOn(currentPosition)
+                    }
+            }
+        }
+
         return new
     }
 
-    fun neighbors(x: Int, y: Int): Int {
-        var on = if (on(x, y)) -1 else 0
+    private fun neighbors(position: Position2d<Int>): Int {
+        var on = if (on(position.x, position.y)) -1 else 0
+
         for (dy in -1..1) for (dx in -1..1) {
-            if (on(x + dx, y + dy)) on++
+            if (on(position.x + dx, position.y + dy)) {
+                on++
+            }
         }
+
         return on
     }
 
     fun turnCornersOn(): BooleanGrid {
-        turnOn(0, 0)
-        turnOn(size - 1, 0)
-        turnOn(0, size - 1)
-        turnOn(size - 1, size - 1)
+        turnOn(Position2d(0, 0))
+        turnOn(Position2d(size - 1, 0))
+        turnOn(Position2d(0, size - 1))
+        turnOn(Position2d(size - 1, size - 1))
         return this
     }
 
     companion object {
         fun from(lines: Collection<String>): BooleanGrid {
             val grid = BooleanGrid(lines.size)
-            lines.forEachIndexed { y, row -> row.forEachIndexed { x, c -> if (c == '#') grid.turnOn(x, y) } }
+
+            lines
+                .forEachIndexed { y, row ->
+                    row.forEachIndexed { x, c ->
+                        if (c == '#') {
+                            grid.turnOn(Position2d(x, y))
+                        }
+                    }
+                }
+
             return grid
         }
     }
--- aoc2015/aoc2015-kotlin/src/main/kotlin/de/havox_design/aoc2015/day18/Grid.kt
+++ aoc2015/aoc2015-kotlin/src/main/kotlin/de/havox_design/aoc2015/day18/Grid.kt
@@ -1,8 +1,10 @@
 package de.havox_design.aoc2015.day18
 
+import de.havox_design.aoc.utils.kotlin.model.positions.Position2d
+
 interface Grid {
-    fun turnOn(x: Int, y: Int)
-    fun turnOff(x: Int, y: Int)
-    fun toggle(x: Int, y: Int)
+    fun turnOn(position: Position2d<Int>)
+    fun turnOff(position: Position2d<Int>)
+    fun toggle(position: Position2d<Int>)
     fun lights(): Int
 }
