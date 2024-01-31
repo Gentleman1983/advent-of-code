a8f2623e5e997d052beb451bfe4ecf3c66f74bf4
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-01-31 07:30:59 +0100

    #28: Optimized AoC2016 days 22

--- aoc2016/aoc2016-kotlin/src/main/kotlin/de/havox_design/aoc2016/day22/Node.kt
+++ aoc2016/aoc2016-kotlin/src/main/kotlin/de/havox_design/aoc2016/day22/Node.kt
@@ -1,5 +1,7 @@
 package de.havox_design.aoc2016.day22
 
-data class Node(val x: Int, val y: Int, val size: Int, val used: Int) {
+import de.havox_design.aoc.utils.kotlin.model.positions.Position2d
+
+data class Node(val position: Position2d<Int>, val size: Int, val used: Int) {
     val available = size - used
 }
