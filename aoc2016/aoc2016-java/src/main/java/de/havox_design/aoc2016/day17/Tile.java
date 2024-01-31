4f7c18ae27b38b3ec697f17bf9b47d0d966242e9
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-01-31 23:26:33 +0100

    #28: Optimized AoC2016 days 17

--- aoc2016/aoc2016-java/src/main/java/de/havox_design/aoc2016/day17/Tile.java
+++ aoc2016/aoc2016-java/src/main/java/de/havox_design/aoc2016/day17/Tile.java
@@ -1,21 +1,22 @@
 package de.havox_design.aoc2016.day17;
 
 import de.havox_design.aoc.utils.kotlin.model.directions.UDLRDirection;
+import de.havox_design.aoc.utils.kotlin.model.positions.Position2d;
 
-public record Tile(int row, int col) {
+public record Tile(Position2d<Integer> position) {
     public boolean isValid(int rowCount, int colCount) {
-        return row >= 0
-                && row < rowCount
-                && col >= 0
-                && col < colCount;
+        return position.getY() >= 0
+                && position.getY() < rowCount
+                && position.getX() >= 0
+                && position.getX() < colCount;
     }
 
     public Tile neighbor(UDLRDirection direction) {
-        return switch (direction) {
-            case UDLRDirection.UP -> new Tile(row - 1, col);
-            case UDLRDirection.DOWN -> new Tile(row + 1, col);
-            case UDLRDirection.LEFT -> new Tile(row, col - 1);
-            case UDLRDirection.RIGHT -> new Tile(row, col + 1);
-        };
+        return new Tile(
+                new Position2d<>(
+                        direction.getDirection().getX() + position.getX(),
+                        direction.getDirection().getY() + position.getY()
+                )
+        );
     }
 }
