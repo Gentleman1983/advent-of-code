e1cb4ac861054099c72b02409d15083f699446b5
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-02-05 23:51:38 +0100

    Solve AoC 2018 Day 6 Part 1

--- aoc2018/aoc2018-java/src/main/java/de/havox_design/aoc2018/day06/ChronalCoordinates.java
+++ aoc2018/aoc2018-java/src/main/java/de/havox_design/aoc2018/day06/ChronalCoordinates.java
@@ -1,12 +1,19 @@
 package de.havox_design.aoc2018.day06;
 
 import de.havox_design.aoc.utils.java.AoCFunctionality;
+import de.havox_design.aoc.utils.kotlin.model.positions.Position2d;
+
+import java.util.*;
 
 public class ChronalCoordinates implements AoCFunctionality {
-    private final String input;
+    private static final String ILLEGAL_STATE_MESSAGE = "This should never happen...";
+
+    private final List<Position2d<Integer>> input;
 
     public ChronalCoordinates(String fileName) {
-        input = readString(fileName);
+        CoordinateParser parser = new CoordinateParser();
+
+        input = parser.parse(readData(fileName));
     }
 
     public static long processTask1(String fileName) {
@@ -20,10 +27,105 @@ public class ChronalCoordinates implements AoCFunctionality {
     }
 
     public long processTask1() {
-        return 17L;
+        int maxX = input
+                .stream()
+                .mapToInt(Position2d::getX)
+                .max()
+                .orElseThrow(() -> new IllegalStateException(ILLEGAL_STATE_MESSAGE));
+        int maxY = input
+                .stream()
+                .mapToInt(Position2d::getY)
+                .max()
+                .orElseThrow(() -> new IllegalStateException(ILLEGAL_STATE_MESSAGE));
+
+        Position2d<Integer>[][] map = calculateCoordinateDistances(maxX, maxY);
+        Set<Position2d<Integer>> candidates = calculateNonInfiniteCandidates(maxX, maxY, map);
+        Map<Position2d<Integer>, Long> counts = countCandidates(maxX, maxY, map, candidates);
+
+        return counts
+                .values()
+                .stream()
+                .max(Comparator.naturalOrder())
+                .orElseThrow(() -> new IllegalStateException(ILLEGAL_STATE_MESSAGE));
     }
 
     public long processTask2() {
         return 0;
+    }
+
+    private Position2d<Integer> detectNearestCoordinate(int x, int y) {
+        Map<Position2d<Integer>, Integer> distances = new HashMap<>();
+        for (Position2d<Integer> coordinate : this.input) {
+            int distance = Math.abs(x - coordinate.getX()) + Math.abs(y - coordinate.getY());
+            distances.put(coordinate, distance);
+        }
+
+        int minDistance = distances
+                .values()
+                .stream()
+                .min(Comparator.naturalOrder())
+                .orElseThrow(() -> new IllegalStateException(ILLEGAL_STATE_MESSAGE));
+
+        boolean multipleMinDistances = distances
+               .values()
+                .stream()
+                .filter(distance -> distance == minDistance)
+                .count() > 1;
+
+        Position2d<Integer> minCoordinate = distances
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() == minDistance)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException(ILLEGAL_STATE_MESSAGE));
+
+        return multipleMinDistances ? null : minCoordinate;
+    }
+
+    @SuppressWarnings("unchecked")
+    private Position2d<Integer>[][] calculateCoordinateDistances(int maxX, int maxY) {
+        Position2d<Integer>[][] map = new Position2d[maxX + 1][maxY + 1];
+        for (int x = 0; x <= maxX; x++) {
+            for (int y = 0; y <= maxY; y++) {
+                map[x][y] = this.detectNearestCoordinate(x, y);
+            }
+        }
+        return map;
+    }
+
+    private Set<Position2d<Integer>> calculateNonInfiniteCandidates(int maxX, int maxY, Position2d<Integer>[][] map) {
+        Set<Position2d<Integer>> candidates = new HashSet<>(input);
+        for (int x = 0; x <= maxX; x++) {
+            int minY = 0;
+
+            candidates.remove(map[x][minY]);
+            candidates.remove(map[x][maxY]);
+        }
+        for (int y = 0; y <= maxY; y++) {
+            int minX = 0;
+
+            candidates.remove(map[minX][y]);
+            candidates.remove(map[maxX][y]);
+        }
+        return candidates;
+    }
+
+    private static Map<Position2d<Integer>, Long> countCandidates(
+            int maxX,
+            int maxY,
+            Position2d<Integer>[][] map,
+            Set<Position2d<Integer>> candidates
+    ) {
+        Map<Position2d<Integer>, Long> counts = new HashMap<>();
+        for (int x = 0; x <= maxX; x++) {
+            for (int y = 0; y <= maxY; y++) {
+                Position2d<Integer> coordinate = map[x][y];
+                if (candidates.contains(coordinate)) {
+                    counts.put(coordinate, counts.getOrDefault(coordinate, 0L) + 1);
+                }
+            }
+        }
+        return counts;
     }
 }
