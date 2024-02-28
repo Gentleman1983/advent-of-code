8ee704f17b267c9e5074f27018f9efc706fa333e
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-02-28 23:22:49 +0100

    #34: Conver aoc2016-java to kotlin dsl.

---
+++ aoc2016/aoc2016-java/build.gradle.kts
@@ -0,0 +1,15 @@
+dependencies {
+    api(project(":utils"))
+
+    implementation(libs.commons.codec)
+    implementation(libs.guava)
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.named<Jar>("jar") {
+    manifest {
+        attributes["Implementation-Title"] = project.name
+        attributes["Implementation-Version"] = project.version
+    }
+}
