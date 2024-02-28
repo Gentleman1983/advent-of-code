d47f8db43db6d3fe184bc7219bd5c99a3285921a
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-02-28 23:24:57 +0100

    #34: Conver aoc2018-java to kotlin dsl.

---
+++ aoc2018/aoc2018-java/build.gradle.kts
@@ -0,0 +1,17 @@
+dependencies {
+    api(project(":utils"))
+
+    implementation(libs.commons.codec)
+    implementation(libs.guava)
+
+    testImplementation(libs.equalsverifier)
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.named<Jar>("jar") {
+    manifest {
+        attributes["Implementation-Title"] = project.name
+        attributes["Implementation-Version"] = project.version
+    }
+}
