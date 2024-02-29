999b1eaa3c64e08ee33e12995083aa0a3f136be4
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-02-29 19:15:10 +0100

    #34: Conver aoc2018-kotlin to kotlin dsl.

--- aoc2018/aoc2018-kotlin/build.gradle.kts
+++ aoc2018/aoc2018-kotlin/build.gradle.kts
@@ -3,19 +3,19 @@ plugins {
 }
 
 dependencies {
-    api project(":utils")
+    api(project(":utils"))
 
-    implementation 'org.jetbrains.kotlin:kotlin-reflect'
-    implementation 'org.jetbrains.kotlin:kotlin-stdlib-jdk8'
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    testImplementation 'org.jetbrains.kotlin:kotlin-test-junit'
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 
-    testRuntimeOnly 'org.junit.platform:junit-platform-launcher'
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-jar {
+tasks.named<Jar>("jar") {
     manifest {
-        attributes 'Implementation-Title': project.name
-        attributes 'Implementation-Version': project.version
+        attributes["Implementation-Title"] = project.name
+        attributes["Implementation-Version"] = project.version
     }
 }
