429698ceda3254da765b84ffef75c350a11769d5
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-02-29 19:37:40 +0100

    #34: Convert aoc2020 to kotlin dsl.

--- aoc2020/build.gradle.kts
+++ aoc2020/build.gradle.kts
@@ -1,27 +1,27 @@
 plugins {
-    id 'application'
+    application
     alias(libs.plugins.kotlin.jvm)
 }
 
 dependencies {
-    api project(":aoc2020:aoc2020-kotlin")
+    api(project(":aoc2020:aoc2020-kotlin"))
 
-    implementation 'org.jetbrains.kotlin:kotlin-reflect'
-    implementation 'org.jetbrains.kotlin:kotlin-stdlib-jdk8'
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    testImplementation 'org.jetbrains.kotlin:kotlin-test-junit'
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 
-    testRuntimeOnly 'org.junit.platform:junit-platform-launcher'
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 application {
-    mainClass = 'de.havox_design.aoc2015.MainClass'
+    mainClass = "de.havox_design.aoc2020.MainClass"
 }
 
-jar {
+tasks.named<Jar>("jar") {
     manifest {
-        attributes 'Implementation-Title': project.name
-        attributes 'Implementation-Version': project.version
-        attributes 'Main-Class': 'de.havox_design.aoc2015.MainClass'
+        attributes["Implementation-Title"] = project.name
+        attributes["Implementation-Version"] = project.version
+        attributes["Main-Class"] = "de.havox_design.aoc2020.MainClass"
     }
 }
