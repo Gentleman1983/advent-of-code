121445fb76337eeebc9d3a809f4f08089250fd04
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-03-16 22:59:21 +0100

    Solved AoC2019 Day 4 Part 1

--- aoc2019/aoc2019-java/src/main/java/de/havox_design/aoc2019/day04/SecureContainer.java
+++ aoc2019/aoc2019-java/src/main/java/de/havox_design/aoc2019/day04/SecureContainer.java
@@ -2,11 +2,29 @@ package de.havox_design.aoc2019.day04;
 
 import de.havox_design.aoc.utils.java.AoCFunctionality;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class SecureContainer implements AoCFunctionality {
-    private final String input;
+    private final int rangeFrom;
+    private final int rangeTo;
 
     public SecureContainer(String fileName) {
-        input = readString(fileName);
+        String input = readString(fileName);
+        List<Integer> inputRange = Arrays
+                .stream(input.split("-"))
+                .map(value -> Integer.parseInt(value.trim()))
+                .toList();
+
+        if (inputRange.size() != 2) {
+            throw new IllegalArgumentException(
+                    String.format("Input '%s' does not fit the input format '${RANGEFROM}-${RANGETO}'.", input)
+            );
+        }
+
+        rangeFrom = inputRange.getFirst();
+        rangeTo = inputRange.getLast();
     }
 
     public static long processTask1(String fileName) {
@@ -20,10 +38,33 @@ public class SecureContainer implements AoCFunctionality {
     }
 
     public long processTask1() {
-        return 0;
+        return IntStream
+                .rangeClosed(rangeFrom, rangeTo)
+                .filter(this::isValidPassword)
+                .count();
     }
 
     public long processTask2() {
         return 0;
+    }
+
+    private boolean isValidPassword(int password) {
+        String passwordString = String.valueOf(password);
+        boolean hasTwoSameAdjacentDigits = false;
+        int lastDigit = -1;
+
+        for (int i = 0; i < passwordString.length(); i++) {
+            int currentDigit = passwordString.charAt(i) - '0';
+
+            if (lastDigit == currentDigit) {
+                hasTwoSameAdjacentDigits = true;
+            } else if (lastDigit > currentDigit) {
+                return false;
+            }
+
+            lastDigit = currentDigit;
+        }
+
+        return hasTwoSameAdjacentDigits;
     }
 }
