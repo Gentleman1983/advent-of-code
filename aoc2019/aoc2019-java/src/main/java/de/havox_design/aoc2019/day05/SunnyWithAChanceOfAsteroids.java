3d98f9cdd90209d693c512cea4c009172caf809f
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-03-16 23:52:12 +0100

    Solved AoC2019 Day 5 Part 1

--- aoc2019/aoc2019-java/src/main/java/de/havox_design/aoc2019/day05/SunnyWithAChanceOfAsteroids.java
+++ aoc2019/aoc2019-java/src/main/java/de/havox_design/aoc2019/day05/SunnyWithAChanceOfAsteroids.java
@@ -1,12 +1,25 @@
 package de.havox_design.aoc2019.day05;
 
 import de.havox_design.aoc.utils.java.AoCFunctionality;
+import de.havox_design.aoc.utils.java.model.computer.aoc2019.Computer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class SunnyWithAChanceOfAsteroids implements AoCFunctionality {
-    private final String input;
+    private static final String VALUE_DELIMITER = ",";
+
+    private final List<Long> input;
 
     public SunnyWithAChanceOfAsteroids(String fileName) {
-        input = readString(fileName);
+        input = Arrays
+                .stream(readString(fileName).split(VALUE_DELIMITER))
+                .map(value -> Long.parseLong(value.trim()))
+                .toList();
     }
 
     public static long processTask1(String fileName) {
@@ -20,10 +33,22 @@ public class SunnyWithAChanceOfAsteroids implements AoCFunctionality {
     }
 
     public long processTask1() {
-        return 0;
+        return process(1L);
     }
 
     public long processTask2() {
         return 0;
     }
+
+    private Long process(long inputNumber ) {
+        BlockingQueue<Long> in = new LinkedBlockingQueue<>();
+        BlockingDeque<Long> out = new LinkedBlockingDeque<>();
+
+        in.add( inputNumber );
+
+        Computer.runComputer( input, in, out, false );
+
+        return out.removeLast();
+    }
+
 }
