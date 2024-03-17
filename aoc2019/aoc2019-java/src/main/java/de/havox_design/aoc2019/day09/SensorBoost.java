739701a961ff943437cd60b4405c5d5248edd7da
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-03-17 18:01:52 +0100

    Solved AoC2019 Day 9 Part 1

--- aoc2019/aoc2019-java/src/main/java/de/havox_design/aoc2019/day09/SensorBoost.java
+++ aoc2019/aoc2019-java/src/main/java/de/havox_design/aoc2019/day09/SensorBoost.java
@@ -1,15 +1,30 @@
 package de.havox_design.aoc2019.day09;
 
 import de.havox_design.aoc.utils.java.AoCFunctionality;
+import de.havox_design.aoc.utils.java.model.computer.aoc2019.Computer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static java.util.stream.Collectors.joining;
 
 public class SensorBoost implements AoCFunctionality {
-    private final String input;
+    private static final String VALUE_DELIMITER = ",";
+
+    private final List<Long> input;
 
     public SensorBoost(String fileName) {
-        input = readString(fileName);
+        input = Arrays
+                .stream(readString(fileName).split(VALUE_DELIMITER))
+                .map(value -> Long.parseLong(value.trim()))
+                .toList();
     }
 
-    public static long processTask1(String fileName) {
+    public static String processTask1(String fileName) {
         SensorBoost instance = new SensorBoost(fileName);
         return instance.processTask1();
     }
@@ -19,11 +34,24 @@ public class SensorBoost implements AoCFunctionality {
         return instance.processTask2();
     }
 
-    public long processTask1() {
-        return 0;
+    public String processTask1() {
+        return compute(1L);
     }
 
     public long processTask2() {
         return 0;
+    }
+
+    private String compute(long inputNumber ) {
+        BlockingQueue<Long> in = new LinkedBlockingQueue<>();
+        BlockingDeque<Long> out = new LinkedBlockingDeque<>();
+
+        in.add( inputNumber );
+        Computer.runComputer( input, in, out, false );
+
+        return out
+                .stream()
+                .map( String::valueOf )
+                .collect( joining( "," ) );
     }
 }
--- aoc2019/aoc2019-java/src/test/java/de/havox_design/aoc2019/day09/Day09Test.java
+++ aoc2019/aoc2019-java/src/test/java/de/havox_design/aoc2019/day09/Day09Test.java
@@ -11,13 +11,15 @@ class Day09Test {
 
     @ParameterizedTest
     @MethodSource("getDataForTask1")
-    void testTask1(String fileName, long expectation) {
+    void testTask1(String fileName, String expectation) {
         Assertions.assertEquals(expectation, SensorBoost.processTask1(fileName));
     }
 
     private static Stream<Arguments> getDataForTask1() {
         return Stream.of(
-                Arguments.of("de/havox_design/aoc2019/day09/day09part1sample1.txt", 0L)
+                Arguments.of("de/havox_design/aoc2019/day09/day09part1sample1.txt", "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"),
+                Arguments.of("de/havox_design/aoc2019/day09/day09part1sample2.txt", "1219070632396864"),
+                Arguments.of("de/havox_design/aoc2019/day09/day09part1sample3.txt", "1125899906842624")
         );
     }
 
