99431ddb6e7c942b6a8167328796c1d2a5548427
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-04-08 23:12:18 +0200

    Solved AoC2019 Day 20 Part 1

--- aoc2019/aoc2019-java/src/main/java/de/havox_design/aoc2019/day20/DonutMaze.java
+++ aoc2019/aoc2019-java/src/main/java/de/havox_design/aoc2019/day20/DonutMaze.java
@@ -1,12 +1,23 @@
 package de.havox_design.aoc2019.day20;
 
+import static de.havox_design.aoc2019.day20.Pair.ZERO;
+import static java.lang.Character.isLetter;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import de.havox_design.aoc.utils.java.AoCFunctionality;
 
+import java.util.*;
+import java.util.stream.Stream;
+
 public class DonutMaze implements AoCFunctionality {
-    private final String input;
+    private static final char ICON_PASSAGE = '.';
+    private static final char ICON_WALL = '#';
+
+    private final List<String> input;
 
     public DonutMaze(String fileName) {
-        input = readString(fileName);
+        input = readData(fileName);
     }
 
     public static long processTask1(String fileName) {
@@ -20,10 +31,211 @@ public class DonutMaze implements AoCFunctionality {
     }
 
     public long processTask1() {
-        return 0;
+        Pair<Pair<Long, Long>, Long> source = new Pair<>(ZERO, 0L);
+        Pair<Pair<Long, Long>, Long> destination = new Pair<>(ZERO, 0L);
+        Multimap<Pair<Long, Long>, Pair<Pair<Long, Long>, Long>> edges = initialize(source, destination);
+
+        return computeDistance(edges, source, destination);
     }
 
     public long processTask2() {
         return 0;
+    }
+
+    private long computeDistance(
+            final Multimap<Pair<Long, Long>, Pair<Pair<Long, Long>, Long>> edges,
+            final Pair<Pair<Long, Long>, Long> source,
+            final Pair<Pair<Long, Long>, Long> destination
+    ) {
+        Queue<Pair<Pair<Long, Long>, Long>> queue = new LinkedList<>();
+        Map<Pair<Pair<Long, Long>, Long>, Long> distances = new HashMap<>();
+
+        queue.add(source);
+        distances.put(source, 0L);
+
+        while (!queue.isEmpty()) {
+            Pair<Pair<Long, Long>, Long> current = queue.remove();
+
+            if (current.equals(destination)) {
+                return distances.get(current);
+            }
+
+            for (Pair<Pair<Long, Long>, Long> n : edges.get(current.getFirst())) {
+                long floor = current.getSecond() + n.getSecond();
+
+                if (floor == -1) {
+                    continue;
+                }
+
+                Pair<Pair<Long, Long>, Long> cell = new Pair<>(n.getFirst(), floor);
+
+                if (!distances.containsKey(cell)) {
+                    queue.add(cell);
+                    distances.put(cell, distances.get(current) + 1);
+                }
+            }
+        }
+
+        throw new IllegalStateException();
+    }
+
+    private Multimap<Pair<Long, Long>, Pair<Pair<Long, Long>, Long>> initialize(
+            Pair<Pair<Long, Long>, Long> source,
+            Pair<Pair<Long, Long>, Long> destination
+    ) {
+        Map<Pair<Long, Long>, Character> map = initializeMap();
+        long maxX = map
+                .keySet()
+                .stream()
+                .mapToLong(Pair::getFirst)
+                .max()
+                .orElseThrow();
+        long maxY = map
+                .keySet()
+                .stream()
+                .mapToLong(Pair::getSecond)
+                .max()
+                .orElseThrow();
+        Multimap<Pair<Long, Long>, Pair<Pair<Long, Long>, Long>> edges = HashMultimap.create();
+        Map<String, Pair<Long, Long>> portals = new HashMap<>();
+
+        for (Map. Entry<Pair<Long, Long>, Character> p : map.entrySet()) {
+            if (p.getValue() == ICON_PASSAGE) {
+                for (Pair<Long, Long> n : computeNeighbours(p.getKey(), map)) {
+                    edges.put(p.getKey(), new Pair<>(n, 0L));
+                    edges.put(n, new Pair<>(p.getKey(), 0L));
+                }
+            } else if (isLetter(p.getValue())) {
+                addPortal(p, map, portals, edges, source, destination, maxX, maxY);
+            }
+        }
+
+        return edges;
+    }
+
+    private Map<Pair<Long, Long>, Character> initializeMap() {
+        Map<Pair<Long, Long>, Character> map = new HashMap<>();
+        Pair<Long, Long> current = new Pair<>(0L, 0L);
+
+        input
+                .forEach(
+                        line -> {
+                            current.setFirst(0L);
+
+                            for (Character c : line.toCharArray()) {
+                                map.put(new Pair<>(current.getFirst(), current.getSecond()), c);
+                                current.setFirst(current.getFirst() + 1);
+                            }
+
+                            current.setSecond(current.getSecond() + 1);
+                        }
+                );
+
+        return map;
+    }
+
+    private void addPortal(
+            Map.Entry<Pair<Long, Long>, Character> p,
+            Map<Pair<Long, Long>, Character> map,
+            Map<String, Pair<Long, Long>> portals,
+            Multimap<Pair<Long, Long>, Pair<Pair<Long, Long>, Long>> edges,
+            Pair<Pair<Long, Long>, Long> source,
+            Pair<Pair<Long, Long>, Long> destination,
+            long maxX,
+            long maxY
+    ) {
+        Pair<Long, Long> pos = p.getKey();
+        Character c = p.getValue();
+        String portal = getPortalName(map, pos, c);
+        List<Pair<Long, Long>> cells = computeNeighbours(pos, map);
+
+        if (!cells.isEmpty()) {
+            Pair<Long, Long> cell = cells.getFirst();
+
+            if (portals.containsKey(portal)) {
+                Pair<Long, Long> in;
+                Pair<Long, Long> out;
+
+                if (isOutPortal(cell, maxX, maxY)) {
+                    out = cell;
+                    in = portals.get(portal);
+                } else {
+                    out = portals.get(portal);
+                    in = cell;
+                }
+
+                long direction = 0;
+
+                edges.put(in, new Pair<>(out, direction));
+                edges.put(out, new Pair<>(in, -direction));
+            } else if (portal.equals("AA")) {
+                source.setFirst(cell);
+                source.setSecond(0L);
+            } else if (portal.equals("ZZ")) {
+                destination.setFirst(cell);
+                destination.setSecond(0L);
+            } else {
+                portals.put(portal, cell);
+            }
+        }
+    }
+
+    private boolean isOutPortal(Pair<Long, Long> cell, long maxX, long maxY) {
+        long x = cell.getFirst();
+        long y = cell.getSecond();
+
+        return x == 2 || y == 2 || x == maxX - 2 || y == maxY - 2;
+    }
+
+    private String getPortalName(
+            Map<Pair<Long, Long>, Character> map,
+            Pair<Long, Long> position,
+            Character c
+    ) {
+        Stream<String> a = Stream
+                .of(
+                        new Pair<>(position.getFirst() + 1, position.getSecond()),
+                        new Pair<>(position.getFirst(), position.getSecond() + 1)
+                )
+                .filter(n -> isPortal(n, map))
+                .map(map::get)
+                .map(n -> concat(c, n));
+        Stream<String> b = Stream
+                .of(
+                        new Pair<>(position.getFirst() - 1, position.getSecond()),
+                        new Pair<>(position.getFirst(), position.getSecond() - 1)
+                )
+                .filter(n -> isPortal(n, map))
+                .map(map::get)
+                .map(n -> concat(n, c));
+
+        return Stream
+                .concat(a, b)
+                .findFirst()
+                .orElseThrow();
+    }
+
+    private String concat(Character a, Character b) {
+        return String.valueOf(a) + b;
+    }
+
+    private List<Pair<Long, Long>> computeNeighbours(Pair<Long, Long> pos, Map<Pair<Long, Long>, Character> map) {
+        return Stream
+                .of(
+                        new Pair<>(pos.getFirst() + 1, pos.getSecond()),
+                        new Pair<>(pos.getFirst() - 1, pos.getSecond()),
+                        new Pair<>(pos.getFirst(), pos.getSecond() - 1),
+                        new Pair<>(pos.getFirst(), pos.getSecond() + 1)
+                )
+                .filter(n -> isCell(n, map))
+                .toList();
+    }
+
+    private boolean isCell(Pair<Long, Long> n, final Map<Pair<Long, Long>, Character> map) {
+        return map.getOrDefault(n, ICON_WALL) == ICON_PASSAGE;
+    }
+
+    private boolean isPortal(Pair<Long, Long> n, Map<Pair<Long, Long>, Character> map) {
+        return isLetter(map.getOrDefault(n, ICON_WALL));
     }
 }
