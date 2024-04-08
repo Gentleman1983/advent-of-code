5254018a5692abc7099bcb91cbbf51fdd907b954
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-04-08 21:38:22 +0200

    Solved AoC2019 Day 19 Part 1

--- aoc2019/aoc2019-java/src/main/java/de/havox_design/aoc2019/day19/TractorBeam.java
+++ aoc2019/aoc2019-java/src/main/java/de/havox_design/aoc2019/day19/TractorBeam.java
@@ -1,12 +1,26 @@
 package de.havox_design.aoc2019.day19;
 
 import de.havox_design.aoc.utils.java.AoCFunctionality;
+import de.havox_design.aoc.utils.java.exceptions.AdventOfCodeException;
+import de.havox_design.aoc.utils.java.model.computer.aoc2019.IntComputer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class TractorBeam implements AoCFunctionality {
-    private final String input;
+    private static final String VALUE_DELIMITER = ",";
+
+    private final List<Long> input;
 
     public TractorBeam(String fileName) {
-        input = readString(fileName);
+        input = Arrays
+                .stream(readString(fileName).split(VALUE_DELIMITER))
+                .map(value -> Long.parseLong(value.trim()))
+                .toList();
     }
 
     public static long processTask1(String fileName) {
@@ -20,10 +34,44 @@ public class TractorBeam implements AoCFunctionality {
     }
 
     public long processTask1() {
-        return 0;
+        BlockingQueue<Long> in = new LinkedBlockingQueue<>();
+        BlockingDeque<Long> out = new LinkedBlockingDeque<>();
+        int size = 50;
+        long[][] matrix = new long[size][size];
+
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                matrix[y][x] = getNumber(input, in, out, x, y);
+            }
+        }
+
+        return Arrays
+                .stream( matrix )
+                .flatMapToLong( Arrays::stream )
+                .filter(e -> e == 1L)
+                .count();
     }
 
     public long processTask2() {
         return 0;
+    }
+
+    @SuppressWarnings("squid:S2142")
+    private long getNumber(
+            List<Long> program,
+            BlockingQueue<Long> in,
+            BlockingDeque<Long> out,
+            int x,
+            int y
+    ) {
+        in.add((long) x);
+        in.add((long) y);
+
+        try {
+            IntComputer.runComputer(program, in, out, false);
+            return out.take();
+        } catch (InterruptedException e) {
+            throw new AdventOfCodeException(e);
+        }
     }
 }
