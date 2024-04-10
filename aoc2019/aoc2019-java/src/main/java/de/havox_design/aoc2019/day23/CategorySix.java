3e7a58e757125ef71cbe3d5c74c8d74671716959
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-04-10 22:45:06 +0200

    Solved AoC2019 Day 23 Part 1

--- aoc2019/aoc2019-java/src/main/java/de/havox_design/aoc2019/day23/CategorySix.java
+++ aoc2019/aoc2019-java/src/main/java/de/havox_design/aoc2019/day23/CategorySix.java
@@ -1,12 +1,25 @@
 package de.havox_design.aoc2019.day23;
 
 import de.havox_design.aoc.utils.java.AoCFunctionality;
+import de.havox_design.aoc.utils.java.exceptions.AdventOfCodeException;
+import de.havox_design.aoc.utils.java.model.computer.aoc2019.IntComputer;
+
+import java.util.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.IntStream;
 
 public class CategorySix implements AoCFunctionality {
-    private final String input;
+    private static final String VALUE_DELIMITER = ",";
+    private static final int COMPUTERS = 50;
+
+    private final List<Long> input;
 
     public CategorySix(String fileName) {
-        input = readString(fileName);
+        input = Arrays
+                .stream(readString(fileName).split(VALUE_DELIMITER))
+                .map(value -> Long.parseLong(value.trim()))
+                .toList();
     }
 
     public static long processTask1(String fileName) {
@@ -20,10 +33,69 @@ public class CategorySix implements AoCFunctionality {
     }
 
     public long processTask1() {
-        return 0;
+        return computePacketValueTo(255);
     }
 
     public long processTask2() {
         return 0;
+    }
+
+    private long computePacketValueTo(int targetAdress) {
+        Map<Integer, BlockingQueue<Long>> in = new HashMap<>();
+        List<BlockingQueue<Long>> out = new ArrayList<>();
+
+        for (int i = 0; i < COMPUTERS; i++) {
+            in.put(i, new LinkedBlockingQueue<>());
+            out.add(new LinkedBlockingQueue<>());
+            in.get(i).add((long) i);
+            new IntComputer(input, in.get(i), out.get(i)).runAsync();
+        }
+
+        in.put(targetAdress, new LinkedBlockingQueue<>());
+
+        return runComputers(in, out, targetAdress);
+    }
+
+    private long runComputers(Map<Integer, BlockingQueue<Long>> receivers, List<BlockingQueue<Long>> senders, int targetAdress) {
+        while (true) {
+            IntStream.range(0, COMPUTERS)
+                    .parallel()
+                    .forEach(to -> receivers.get(to).add(-1L));
+
+            for (int from = 0; from < COMPUTERS; from++) {
+                BlockingQueue<Long> sender = senders.get(from);
+                Integer to = Optional
+                        .ofNullable(sender.poll())
+                        .map(Long::intValue)
+                        .orElse(null);
+
+                if (to != null) {
+                    if (to == targetAdress) {
+                        receivers.get(to).clear();
+                    }
+
+                    long y = dispatchPacket(from, to, receivers, sender);
+
+                    if (to == targetAdress) {
+                        return y;
+                    }
+                }
+            }
+        }
+    }
+
+    private long dispatchPacket(int from, int to, Map<Integer, BlockingQueue<Long>> receivers, BlockingQueue<Long> sender) {
+        try {
+            BlockingQueue<Long> receiver = receivers.get(to);
+            long x = sender.take();
+            long y = sender.take();
+
+            receiver.add(x);
+            receiver.add(y);
+
+            return y;
+        } catch (InterruptedException e) {
+            throw new AdventOfCodeException(e);
+        }
     }
 }
