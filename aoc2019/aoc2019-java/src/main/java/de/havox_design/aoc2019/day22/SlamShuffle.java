707df5160f34f1418518d789f257bc7cdcf27676
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-04-10 22:00:03 +0200

    Solved AoC2019 Day 22 Part 1

--- aoc2019/aoc2019-java/src/main/java/de/havox_design/aoc2019/day22/SlamShuffle.java
+++ aoc2019/aoc2019-java/src/main/java/de/havox_design/aoc2019/day22/SlamShuffle.java
@@ -2,11 +2,18 @@ package de.havox_design.aoc2019.day22;
 
 import de.havox_design.aoc.utils.java.AoCFunctionality;
 
+import java.util.List;
+
 public class SlamShuffle implements AoCFunctionality {
-    private final String input;
+    private static final String INSTRUCTION_CUT_N = "cut";
+    private static final String INSTRUCTION_DEAL_INTO_NEW_STACK = "deal into new stack";
+    private static final String INSTRUCTION_DEAL_WITH_INCREMENT = "deal with increment";
+    private static final char INSTRUCTION_VALUE_DELIMITER = ' ';
+
+    private final List<String> input;
 
     public SlamShuffle(String fileName) {
-        input = readString(fileName);
+        input = readData(fileName);
     }
 
     public static long processTask1(String fileName) {
@@ -20,10 +27,27 @@ public class SlamShuffle implements AoCFunctionality {
     }
 
     public long processTask1() {
-        return 0;
+        int targetIndex = 2019;
+        int numberOfCards = 10007;
+
+        for (String instruction : input) {
+            if (instruction.contains(INSTRUCTION_DEAL_INTO_NEW_STACK)) {
+                targetIndex = numberOfCards - targetIndex - 1;
+            } else if (instruction.contains(INSTRUCTION_DEAL_WITH_INCREMENT)) {
+                targetIndex = Math.floorMod(targetIndex * extractInstructionValue(instruction), numberOfCards);
+            } else if (instruction.contains(INSTRUCTION_CUT_N)) {
+                targetIndex = Math.floorMod(targetIndex + (extractInstructionValue(instruction) * (-1)), numberOfCards);
+            }
+        }
+
+        return targetIndex;
     }
 
     public long processTask2() {
         return 0;
+    }
+
+    private int extractInstructionValue(String instruction) {
+        return Integer.parseInt(instruction.substring(instruction.lastIndexOf(INSTRUCTION_VALUE_DELIMITER) + 1));
     }
 }
