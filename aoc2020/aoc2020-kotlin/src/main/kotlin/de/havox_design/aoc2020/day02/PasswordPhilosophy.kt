9d401fc4934eeef71f72bdfc1188f0e3aa1cefee
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-04-19 23:40:56 +0200

    Solved AoC2020 Day 2 Part 1

--- aoc2020/aoc2020-kotlin/src/main/kotlin/de/havox_design/aoc2020/day02/PasswordPhilosophy.kt
+++ aoc2020/aoc2020-kotlin/src/main/kotlin/de/havox_design/aoc2020/day02/PasswordPhilosophy.kt
@@ -4,10 +4,27 @@ class PasswordPhilosophy(private var filename: String) {
     private val data = getResourceAsText(filename)
 
     fun processPart1(): Any =
-        0L
+        data
+            .count { passwordPolicyAMatches(it) }
 
     fun processPart2(): Any =
         0L
+
+    private fun passwordPolicyAMatches(it: String): Boolean {
+        val (policyMin, policyMax, letter, _, password) = it
+            .split(' ', ':', '-')
+            .map { it.trim() }
+        val letterCount = password
+            .count { it == letter[0] }
+
+        return letterCount >= policyMin.toInt() && letterCount <= policyMax.toInt();
+    }
+
     private fun getResourceAsText(path: String): List<String> =
-        this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
+        this
+            .javaClass
+            .classLoader
+            .getResourceAsStream(path)!!
+            .bufferedReader()
+            .readLines()
 }
--- aoc2020/aoc2020-kotlin/src/test/kotlin/de/havox_design/aoc2020/day02/Day02Test.kt
+++ aoc2020/aoc2020-kotlin/src/test/kotlin/de/havox_design/aoc2020/day02/Day02Test.kt
@@ -10,7 +10,7 @@ class Day02Test {
 
     @ParameterizedTest
     @MethodSource("getDataForTestProcessPart1")
-    fun testProcessPart1(filename: String, expectedResult: Long) =
+    fun testProcessPart1(filename: String, expectedResult: Int) =
         PasswordPhilosophy(filename).processPart1().shouldBe(expectedResult)
 
     @ParameterizedTest
@@ -22,10 +22,10 @@ class Day02Test {
         @JvmStatic
         private fun getDataForTestProcessPart1(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2020/day02/day02part1sample1.txt", 2L),
-                Arguments.of("de/havox_design/aoc2020/day02/day02part1sample2.txt", 1L),
-                Arguments.of("de/havox_design/aoc2020/day02/day02part1sample3.txt", 0L),
-                Arguments.of("de/havox_design/aoc2020/day02/day02part1sample4.txt", 1L)
+                Arguments.of("de/havox_design/aoc2020/day02/day02part1sample1.txt", 2),
+                Arguments.of("de/havox_design/aoc2020/day02/day02part1sample2.txt", 1),
+                Arguments.of("de/havox_design/aoc2020/day02/day02part1sample3.txt", 0),
+                Arguments.of("de/havox_design/aoc2020/day02/day02part1sample4.txt", 1)
             )
 
         @JvmStatic
