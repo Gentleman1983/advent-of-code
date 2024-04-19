7c50cbf7cffc0bec9d03ddfaaa58145b770e3633
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-04-19 23:25:01 +0200

    Solved AoC2020 Day 1 Part 1

--- aoc2020/aoc2020-kotlin/src/main/kotlin/de/havox_design/aoc2020/day01/RepairReport.kt
+++ aoc2020/aoc2020-kotlin/src/main/kotlin/de/havox_design/aoc2020/day01/RepairReport.kt
@@ -3,11 +3,34 @@ package de.havox_design.aoc2020.day01
 class RepairReport(private var filename: String) {
     private val data = getResourceAsText(filename)
 
-    fun processPart1(): Any =
-        0L
+    fun processPart1(): Any {
+        val numbers = data
+            .map { it.toLong() }
+            .toSet()
+
+        return findParts(numbers, 2020) ?: throw IllegalStateException("No solution found")
+    }
 
     fun processPart2(): Any =
         0L
+
+    private fun findParts(numbers: Set<Long>, requiredSum: Int): Long? {
+        for (first in numbers) {
+            val second = requiredSum - first
+
+            if (second in numbers) {
+                return first * second
+            }
+        }
+
+        return null
+    }
+
     private fun getResourceAsText(path: String): List<String> =
-        this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
+        this
+            .javaClass
+            .classLoader
+            .getResourceAsStream(path)!!
+            .bufferedReader()
+            .readLines()
 }
