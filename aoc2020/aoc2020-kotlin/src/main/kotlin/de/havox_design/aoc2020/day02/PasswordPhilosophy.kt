b47d801d091ee3d23311973b31b90e3c23bd13b5
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-04-19 23:46:32 +0200

    Solved AoC2020 Day 2 Part 2

--- aoc2020/aoc2020-kotlin/src/main/kotlin/de/havox_design/aoc2020/day02/PasswordPhilosophy.kt
+++ aoc2020/aoc2020-kotlin/src/main/kotlin/de/havox_design/aoc2020/day02/PasswordPhilosophy.kt
@@ -8,7 +8,8 @@ class PasswordPhilosophy(private var filename: String) {
             .count { passwordPolicyAMatches(it) }
 
     fun processPart2(): Any =
-        0L
+        data
+            .count { passwordPolicyBMatches(it) }
 
     private fun passwordPolicyAMatches(it: String): Boolean {
         val (policyMin, policyMax, letter, _, password) = it
@@ -18,6 +19,14 @@ class PasswordPhilosophy(private var filename: String) {
             .count { it == letter[0] }
 
         return letterCount >= policyMin.toInt() && letterCount <= policyMax.toInt();
+    }
+
+    private fun passwordPolicyBMatches(it: String): Boolean {
+        val (policyFirst, policyLast, letter, _, password) = it
+            .split(' ', ':', '-')
+            .map { it.trim() }
+
+        return (password[policyFirst.toInt() - 1] == letter[0]) xor (password[policyLast.toInt() - 1] == letter[0])
     }
 
     private fun getResourceAsText(path: String): List<String> =
