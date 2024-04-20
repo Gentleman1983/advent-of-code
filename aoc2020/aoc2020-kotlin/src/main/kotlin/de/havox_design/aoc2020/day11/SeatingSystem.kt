821013312390b92a0d35dd94f3e5245437079959
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-04-20 23:27:33 +0200

    Solved AoC2020 Day 11 Part 1

--- aoc2020/aoc2020-kotlin/src/main/kotlin/de/havox_design/aoc2020/day11/SeatingSystem.kt
+++ aoc2020/aoc2020-kotlin/src/main/kotlin/de/havox_design/aoc2020/day11/SeatingSystem.kt
@@ -4,11 +4,28 @@ class SeatingSystem(private var filename: String) {
     private val data = getResourceAsText(filename)
 
     fun processPart1(): Any =
-        0L
+        Seats(
+            data
+                .map { it.toCharArray() }
+                .toTypedArray()
+        )
+            .simulateUntilStable(Seats::calculateOccupiedSeats)
+            .count(ICON_OCCUPIED_SEAT)
 
     fun processPart2(): Any =
         0L
 
     private fun getResourceAsText(path: String): List<String> =
-        this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
+        this
+            .javaClass
+            .classLoader
+            .getResourceAsStream(path)!!
+            .bufferedReader()
+            .readLines()
+
+    companion object {
+        const val ICON_EMPTY_SEAT = 'L'
+        const val ICON_FLOOR = '.'
+        const val ICON_OCCUPIED_SEAT = '#'
+    }
 }
--- aoc2020/aoc2020-kotlin/src/test/kotlin/de/havox_design/aoc2020/day11/Day11Test.kt
+++ aoc2020/aoc2020-kotlin/src/test/kotlin/de/havox_design/aoc2020/day11/Day11Test.kt
@@ -10,7 +10,7 @@ class Day11Test {
 
     @ParameterizedTest
     @MethodSource("getDataForTestProcessPart1")
-    fun testProcessPart1(filename: String, expectedResult: Long) =
+    fun testProcessPart1(filename: String, expectedResult: Int) =
         SeatingSystem(filename).processPart1().shouldBe(expectedResult)
 
     @ParameterizedTest
@@ -22,7 +22,7 @@ class Day11Test {
         @JvmStatic
         private fun getDataForTestProcessPart1(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2020/day11/day11part1sample.txt", 37L)
+                Arguments.of("de/havox_design/aoc2020/day11/day11part1sample.txt", 37)
             )
 
         @JvmStatic
