9abb7a3ff32d637a88afabcaef850c3bd0596969
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-04-20 13:58:12 +0200

    Solved AoC2020 Day 9 Part 2

--- aoc2020/aoc2020-kotlin/src/main/kotlin/de/havox_design/aoc2020/day09/EncodingError.kt
+++ aoc2020/aoc2020-kotlin/src/main/kotlin/de/havox_design/aoc2020/day09/EncodingError.kt
@@ -3,14 +3,46 @@ package de.havox_design.aoc2020.day09
 class EncodingError(private var filename: String) {
     private val data = getResourceAsText(filename)
 
-    fun processPart1(preambleSize: Int = 25): Any =
-        data.map { it.toLong() }
+    fun processPart1(preambleSize: Int = 25): Long =
+        data
+            .map { it.toLong() }
             .windowed(preambleSize + 1, partialWindows = false)
             .first { cantMakeFromParts(it) }
             .last()
 
-    fun processPart2(): Any =
-        0L
+    fun processPart2(preambleSize: Int = 25): Any {
+        val nums = data
+            .map { it.toLong() }
+        val bad = nums
+            .windowed(preambleSize + 1)
+            .map { it.dropLast(1) to it.last() }
+            .map { (head, tail) -> head.flatMapIndexed { i, x -> head.drop(i + 1).map { it + x } }.toSet() to tail }
+            .first { (head, tail) -> tail !in head }
+            .second
+        val partials = nums
+            .scan(0L) { acc, value -> acc + value }
+        var from = 0
+        var to = 2
+
+        while (to in partials.indices) {
+            val curr = partials[to] - partials[from]
+
+            when {
+                curr < bad -> to++
+                curr > bad -> {
+                    from++
+                    to = maxOf(to, from + 2)
+                }
+
+                else -> break
+            }
+        }
+
+        val subset = nums
+            .subList(from, to)
+
+        return subset.minOf { it } + subset.maxOf { it }
+    }
 
     private fun cantMakeFromParts(numbers: List<Long>): Boolean {
         val total = numbers.last()
@@ -25,5 +57,10 @@ class EncodingError(private var filename: String) {
     }
 
     private fun getResourceAsText(path: String): List<String> =
-        this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
+        this
+            .javaClass
+            .classLoader
+            .getResourceAsStream(path)!!
+            .bufferedReader()
+            .readLines()
 }
--- aoc2020/aoc2020-kotlin/src/test/kotlin/de/havox_design/aoc2020/day09/Day09Test.kt
+++ aoc2020/aoc2020-kotlin/src/test/kotlin/de/havox_design/aoc2020/day09/Day09Test.kt
@@ -16,19 +16,19 @@ class Day09Test {
     @ParameterizedTest
     @MethodSource("getDataForTestProcessPart2")
     fun testProcessPart2(filename: String, expectedResult: Long) =
-        EncodingError(filename).processPart2().shouldBe(expectedResult)
+        EncodingError(filename).processPart2(5).shouldBe(expectedResult)
 
     companion object {
         @JvmStatic
         private fun getDataForTestProcessPart1(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2020/day09/day09part1sample.txt", 127L)
+                Arguments.of("de/havox_design/aoc2020/day09/day09sample.txt", 127L)
             )
 
         @JvmStatic
         private fun getDataForTestProcessPart2(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2020/day09/day09part2sample.txt", 62L)
+                Arguments.of("de/havox_design/aoc2020/day09/day09sample.txt", 62L)
             )
     }
 }
