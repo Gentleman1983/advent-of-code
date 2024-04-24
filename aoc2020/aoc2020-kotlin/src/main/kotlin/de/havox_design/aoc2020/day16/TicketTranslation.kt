6f2d96689f97fe0ef67813f6bebb0f41d751bb80
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-04-24 20:34:11 +0200

    Solved AoC2020 Day 16 Part 1

--- aoc2020/aoc2020-kotlin/src/main/kotlin/de/havox_design/aoc2020/day16/TicketTranslation.kt
+++ aoc2020/aoc2020-kotlin/src/main/kotlin/de/havox_design/aoc2020/day16/TicketTranslation.kt
@@ -3,12 +3,48 @@ package de.havox_design.aoc2020.day16
 class TicketTranslation(private var filename: String) {
     private val data = getResourceAsText(filename)
 
-    fun processPart1(): Any =
-        0L
+    fun processPart1(): Any {
+        val fieldRules = extractFieldRules(data)
+
+        val nearbyTickets = data
+            .subList(data.indexOf("nearby tickets:") + 1, data.size)
+
+        return nearbyTickets
+            .asSequence()
+            .flatMap { it.split(TICKET_DELIMITER).map(String::toInt) }
+            .filter { value -> !fieldRules.values.any(value::let) }
+            .sum()
+    }
 
     fun processPart2(): Any =
         0L
 
+    private fun extractFieldRules(allLines: List<String>): Map<String, (Int) -> Boolean> =
+        allLines
+            .takeWhile(String::isNotBlank)
+            .associate {
+                val key = it
+                    .substringBefore(CLASS_DELIMITER)
+                val (a, b, c, d) = RANGE_PATTERN
+                    .matchEntire(it.substringAfter(CLASS_DELIMITER))!!
+                    .destructured
+
+                key to { value: Int -> value in (a.toInt()..b.toInt()) || value in (c.toInt()..d.toInt()) }
+            }
+
     private fun getResourceAsText(path: String): List<String> =
-        this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
+        this
+            .javaClass
+            .classLoader
+            .getResourceAsStream(path)!!
+            .bufferedReader()
+            .readLines()
+
+    companion object {
+        private const val CLASS_DELIMITER = ": "
+        private const val TICKET_DELIMITER = ","
+
+        private val RANGE_PATTERN = """(\d+)-(\d+) or (\d+)-(\d+)"""
+            .toRegex()
+    }
 }
