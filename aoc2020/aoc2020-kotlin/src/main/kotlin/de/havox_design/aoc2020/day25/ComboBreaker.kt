208795804a19623f90952570a0755a9ca9ab1e7d
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-04-25 23:15:38 +0200

    Solved AoC2020 Day 25 Part 1

--- aoc2020/aoc2020-kotlin/src/main/kotlin/de/havox_design/aoc2020/day25/ComboBreaker.kt
+++ aoc2020/aoc2020-kotlin/src/main/kotlin/de/havox_design/aoc2020/day25/ComboBreaker.kt
@@ -3,12 +3,32 @@ package de.havox_design.aoc2020.day25
 class ComboBreaker(private var filename: String) {
     private val data = getResourceAsText(filename)
 
-    fun processPart1(): Any =
-        0L
+    fun processPart1(): Any {
+        val items = data
+            .map { value ->
+                value
+                    .trim()
+                    .toLong()
+            }
+        val loop = generateSequence(1L) { (it * 7L) % MODULO }
+            .indexOfFirst { it == items[0] }
+
+        return (0..<loop)
+            .fold(1L) { acc, _ -> (acc * items[1]) % MODULO }
+    }
 
     fun processPart2(): Any =
         0L
 
     private fun getResourceAsText(path: String): List<String> =
-        this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
+        this
+            .javaClass
+            .classLoader
+            .getResourceAsStream(path)!!
+            .bufferedReader()
+            .readLines()
+
+    companion object {
+        private const val MODULO = 20201227L
+    }
 }
