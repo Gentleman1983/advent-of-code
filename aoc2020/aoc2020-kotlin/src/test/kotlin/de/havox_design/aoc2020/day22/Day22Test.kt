5c48129ab232dab2701df91ccfa2f30dec7f8f4b
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-04-25 22:07:22 +0200

    Added AoC2020 Day 22 Part 2

--- aoc2020/aoc2020-kotlin/src/test/kotlin/de/havox_design/aoc2020/day22/Day22Test.kt
+++ aoc2020/aoc2020-kotlin/src/test/kotlin/de/havox_design/aoc2020/day22/Day22Test.kt
@@ -15,7 +15,7 @@ class Day22Test {
 
     @ParameterizedTest
     @MethodSource("getDataForTestProcessPart2")
-    fun testProcessPart2(filename: String, expectedResult: Long) =
+    fun testProcessPart2(filename: String, expectedResult: Int) =
         CrabCombat(filename).processPart2().shouldBe(expectedResult)
 
     companion object {
@@ -28,7 +28,7 @@ class Day22Test {
         @JvmStatic
         private fun getDataForTestProcessPart2(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2020/day22/day22part2sample.txt", 0L)
+                Arguments.of("de/havox_design/aoc2020/day22/day22part2sample.txt", 291)
             )
     }
 }
