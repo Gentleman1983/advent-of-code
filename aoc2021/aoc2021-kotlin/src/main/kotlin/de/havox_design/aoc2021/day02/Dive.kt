e8d8c6a4db2b81c18f3aa4a5aa6bdeee385acd58
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-05-19 11:26:44 +0200

    Solved AoC2021 Day 2 Part 2

--- aoc2021/aoc2021-kotlin/src/main/kotlin/de/havox_design/aoc2021/day02/Dive.kt
+++ aoc2021/aoc2021-kotlin/src/main/kotlin/de/havox_design/aoc2021/day02/Dive.kt
@@ -1,24 +1,34 @@
 package de.havox_design.aoc2021.day02
 
 import de.havox_design.aoc.utils.kotlin.model.positions.Position2d
+import de.havox_design.aoc.utils.kotlin.model.positions.Position3d
 
 class Dive(private var filename: String) {
     private val data = getResourceAsText(filename)
 
     fun processPart1(): Any =
         data
-            .fold(Position2d(0L,0L)) { (x, y), (direction, distance) ->
+            .fold(Position2d(0L, 0L)) { (x, y), (direction, distance) ->
                 when (direction) {
-                    "forward" -> Position2d(x + distance.toInt(), y)
-                    "down" -> Position2d(x, y + distance.toInt())
-                    "up" -> Position2d(x, y - distance.toInt())
+                    "forward" -> Position2d(x + distance.toLong(), y)
+                    "down" -> Position2d(x, y + distance.toLong())
+                    "up" -> Position2d(x, y - distance.toLong())
                     else -> throw IllegalArgumentException("Bad input $direction")
                 }
             }
             .let { (x, y) -> x * y }
 
     fun processPart2(): Any =
-        0L
+        data
+            .fold(Position3d(0L, 0L, 0L)) { (x, y, aim), (direction, distance) ->
+                when (direction) {
+                    "forward" -> Position3d(x + distance.toLong(), y + aim * distance.toLong(), aim)
+                    "down" -> Position3d(x, y, aim + distance.toLong())
+                    "up" -> Position3d(x, y, aim - distance.toLong())
+                    else -> throw IllegalArgumentException("Bad input $direction")
+                }
+            }
+            .let { (x, y, _) -> x * y }
 
     private fun getResourceAsText(path: String): List<List<String>> =
         this
