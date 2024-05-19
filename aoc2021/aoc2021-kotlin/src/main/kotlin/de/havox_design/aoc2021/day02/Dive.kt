7af4e021c38c64391261a596dd364730bdb04259
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-05-19 11:18:30 +0200

    Solved AoC2021 Day 2 Part 1

--- aoc2021/aoc2021-kotlin/src/main/kotlin/de/havox_design/aoc2021/day02/Dive.kt
+++ aoc2021/aoc2021-kotlin/src/main/kotlin/de/havox_design/aoc2021/day02/Dive.kt
@@ -1,14 +1,31 @@
 package de.havox_design.aoc2021.day02
 
+import de.havox_design.aoc.utils.kotlin.model.positions.Position2d
+
 class Dive(private var filename: String) {
     private val data = getResourceAsText(filename)
 
     fun processPart1(): Any =
-        0L
+        data
+            .fold(Position2d(0L,0L)) { (x, y), (direction, distance) ->
+                when (direction) {
+                    "forward" -> Position2d(x + distance.toInt(), y)
+                    "down" -> Position2d(x, y + distance.toInt())
+                    "up" -> Position2d(x, y - distance.toInt())
+                    else -> throw IllegalArgumentException("Bad input $direction")
+                }
+            }
+            .let { (x, y) -> x * y }
 
     fun processPart2(): Any =
         0L
 
-    private fun getResourceAsText(path: String): List<String> =
-        this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
+    private fun getResourceAsText(path: String): List<List<String>> =
+        this
+            .javaClass
+            .classLoader
+            .getResourceAsStream(path)!!
+            .bufferedReader()
+            .readLines()
+            .map { line -> line.split(" ") }
 }
