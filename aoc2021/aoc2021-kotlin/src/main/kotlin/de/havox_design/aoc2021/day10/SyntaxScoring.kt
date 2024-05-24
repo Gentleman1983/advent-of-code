340d4167a0ebb0df9006c83d7bbdd82452fa72a6
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-05-24 22:46:51 +0200

    Added AoC2021 Day 10 Part 2

--- aoc2021/aoc2021-kotlin/src/main/kotlin/de/havox_design/aoc2021/day10/SyntaxScoring.kt
+++ aoc2021/aoc2021-kotlin/src/main/kotlin/de/havox_design/aoc2021/day10/SyntaxScoring.kt
@@ -1,13 +1,47 @@
 package de.havox_design.aoc2021.day10
 
+import java.util.*
+
 class SyntaxScoring(private var filename: String) {
     private val data = getResourceAsText(filename)
+    private val pairs = mapOf(
+        '(' to ')',
+        '<' to '>',
+        '{' to '}',
+        '[' to ']'
+    )
+    private val opening = setOf('(', '{', '<', '[')
+    private val closing = setOf(')', '}', '>', ']')
+    private val syntaxErrorScore = mapOf(
+        ')' to 3,
+        ']' to 57,
+        '}' to 1197,
+        '>' to 25137
+    )
 
     fun processPart1(): Any =
-        0L
+        data
+            .mapNotNull { firstIllegalChar(it) }
+            .sumOf { syntaxErrorScore[it] ?: 0 }
 
     fun processPart2(): Any =
         0L
+
+    private fun firstIllegalChar(line: String): Char? {
+        val stack = Stack<Char>()
+
+        for (it in line) {
+            if (it in opening) {
+                stack.push(it)
+            } else if (it in closing) {
+                val lastOpen = stack.pop()
+                if (it != pairs[lastOpen]) {
+                    return it
+                }
+            }
+        }
+        return null
+    }
 
     private fun getResourceAsText(path: String): List<String> =
         this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
--- aoc2021/aoc2021-kotlin/src/test/kotlin/de/havox_design/aoc2021/day10/Day10Test.kt
+++ aoc2021/aoc2021-kotlin/src/test/kotlin/de/havox_design/aoc2021/day10/Day10Test.kt
@@ -10,25 +10,25 @@ class Day10Test {
 
     @ParameterizedTest
     @MethodSource("getDataForTestProcessPart1")
-    fun testProcessPart1(filename: String, expectedResult: Long) =
+    fun testProcessPart1(filename: String, expectedResult: Int) =
         SyntaxScoring(filename).processPart1().shouldBe(expectedResult)
 
     @ParameterizedTest
     @MethodSource("getDataForTestProcessPart2")
-    fun testProcessPart2(filename: String, expectedResult: Long) =
+    fun testProcessPart2(filename: String, expectedResult: Int) =
         SyntaxScoring(filename).processPart2().shouldBe(expectedResult)
 
     companion object {
         @JvmStatic
         private fun getDataForTestProcessPart1(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2021/day10/day10part1sample.txt", 26397L)
+                Arguments.of("de/havox_design/aoc2021/day10/day10sample.txt", 26397)
             )
 
         @JvmStatic
         private fun getDataForTestProcessPart2(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2021/day10/day10part2sample.txt", 0L)
+                Arguments.of("de/havox_design/aoc2021/day10/day10sample.txt", 288957)
             )
     }
 }
