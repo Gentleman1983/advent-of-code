39ee598ced7313d6649146ba41bf12c461ebd09a
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-05-24 21:26:01 +0200

    Solved AoC2021 Day 7 Part 1

--- aoc2021/aoc2021-kotlin/src/main/kotlin/de/havox_design/aoc2021/day07/TheTreacheryOfWhales.kt
+++ aoc2021/aoc2021-kotlin/src/main/kotlin/de/havox_design/aoc2021/day07/TheTreacheryOfWhales.kt
@@ -1,14 +1,54 @@
 package de.havox_design.aoc2021.day07
 
+import kotlin.math.abs
+
 class TheTreacheryOfWhales(private var filename: String) {
+    private val VALUE_DELIMITER = ","
     private val data = getResourceAsText(filename)
 
-    fun processPart1(): Any =
-        0L
+    fun processPart1(): Any {
+        val positions = data
+            .split(VALUE_DELIMITER)
+            .map { it.toInt() }
+        val (minPos, maxPos) = positions.minAndMax()
+
+        return (minPos..maxPos).minOf { calculateFuel(it, positions) }
+    }
 
     fun processPart2(): Any =
         0L
 
-    private fun getResourceAsText(path: String): List<String> =
-        this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
+    private fun calculateFuel(target: Int, positions: List<Int>): Int =
+        positions
+            .sumOf { abs(target - it) }
+
+    @SuppressWarnings("kotlin:S6532")
+    private fun <T : Comparable<T>> Iterable<T>.minAndMax(): Pair<T, T> {
+        val iterator = iterator()
+        if (!iterator.hasNext()) {
+            throw IllegalArgumentException("Cannot get min and max of empty collection")
+        }
+
+        var min = iterator.next()
+        var max = min
+
+        while (iterator.hasNext()) {
+            val next = iterator.next()
+
+            when {
+                next < min -> min = next
+                next > max -> max = next
+            }
+        }
+
+        return min to max
+    }
+
+    private fun getResourceAsText(path: String): String =
+        this
+            .javaClass
+            .classLoader
+            .getResourceAsStream(path)!!
+            .bufferedReader()
+            .readText()
 }
