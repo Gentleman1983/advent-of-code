cf205b43b265955d9cc52261873c71e56127022e
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-05-24 22:00:32 +0200

    Added AoC2021 Day 8 Part 2

--- aoc2021/aoc2021-kotlin/src/test/kotlin/de/havox_design/aoc2021/day08/Day08Test.kt
+++ aoc2021/aoc2021-kotlin/src/test/kotlin/de/havox_design/aoc2021/day08/Day08Test.kt
@@ -15,7 +15,7 @@ class Day08Test {
 
     @ParameterizedTest
     @MethodSource("getDataForTestProcessPart2")
-    fun testProcessPart2(filename: String, expectedResult: Long) =
+    fun testProcessPart2(filename: String, expectedResult: Int) =
         SevenSegmentSearch(filename).processPart2().shouldBe(expectedResult)
 
     companion object {
@@ -28,7 +28,8 @@ class Day08Test {
         @JvmStatic
         private fun getDataForTestProcessPart2(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2021/day08/day08part2sample.txt", 0L)
+                Arguments.of("de/havox_design/aoc2021/day08/day08part2sample1.txt", 5353),
+                Arguments.of("de/havox_design/aoc2021/day08/day08part2sample1.txt", 61229)
             )
     }
 }
