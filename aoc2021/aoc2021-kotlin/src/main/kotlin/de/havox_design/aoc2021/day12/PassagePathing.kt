cb355524757a5aea0f11574fa3c8ce65169c0737
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-05-25 16:37:19 +0200

    Solved AoC2021 Day 11 Part 2

--- aoc2021/aoc2021-kotlin/src/main/kotlin/de/havox_design/aoc2021/day12/PassagePathing.kt
+++ aoc2021/aoc2021-kotlin/src/main/kotlin/de/havox_design/aoc2021/day12/PassagePathing.kt
@@ -1,14 +1,62 @@
 package de.havox_design.aoc2021.day12
 
 class PassagePathing(private var filename: String) {
+    private val DELIMITER_CONNECTION = "-"
+    private val NODE_NAME_END = "end"
+    private val NODE_NAME_START = "start"
+
     private val data = getResourceAsText(filename)
 
-    fun processPart1(): Any =
-        0L
+    fun processPart1(): Any {
+        val edges = data
+            .map { it.split(DELIMITER_CONNECTION) }
+            .flatMap {
+                listOf(
+                    it.first() to it.last(),
+                    it.last() to it.first()
+                )
+            }
+            .groupBy(
+                keySelector = { it.first },
+                valueTransform = { it.second },
+            )
+            .mapValues { it.value.sorted() }
+
+        val visited = mutableSetOf<List<String>>()
+        val finish = mutableSetOf<List<String>>()
+        val queue = ArrayDeque(listOf(listOf(NODE_NAME_START)))
+
+        while (queue.isNotEmpty()) {
+            val curr = queue.removeFirst()
+
+            if (curr !in visited) {
+                visited += curr
+
+                val tail = curr.last()
+
+                if (tail == NODE_NAME_END) {
+                    finish += curr
+                } else {
+                    queue += edges[tail]
+                        .orEmpty()
+                        .filter { it.uppercase() == it || it !in curr }
+                        .map { curr + it }
+                        .filter { it !in visited }
+                }
+            }
+        }
+
+        return finish.size
+    }
 
     fun processPart2(): Any =
         0L
 
     private fun getResourceAsText(path: String): List<String> =
-        this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
+        this
+            .javaClass
+            .classLoader
+            .getResourceAsStream(path)!!
+            .bufferedReader()
+            .readLines()
 }
