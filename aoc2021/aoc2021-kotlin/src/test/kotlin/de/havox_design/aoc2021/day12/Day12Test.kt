3cf058eaa34f4f2c92322da7a6ce93d66cca5bb9
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-05-25 16:40:18 +0200

    Added AoC2021 Day 12 Part 2

--- aoc2021/aoc2021-kotlin/src/test/kotlin/de/havox_design/aoc2021/day12/Day12Test.kt
+++ aoc2021/aoc2021-kotlin/src/test/kotlin/de/havox_design/aoc2021/day12/Day12Test.kt
@@ -15,22 +15,24 @@ class Day12Test {
 
     @ParameterizedTest
     @MethodSource("getDataForTestProcessPart2")
-    fun testProcessPart2(filename: String, expectedResult: Long) =
+    fun testProcessPart2(filename: String, expectedResult: Int) =
         PassagePathing(filename).processPart2().shouldBe(expectedResult)
 
     companion object {
         @JvmStatic
         private fun getDataForTestProcessPart1(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2021/day12/day12part1sample1.txt", 10),
-                Arguments.of("de/havox_design/aoc2021/day12/day12part1sample2.txt", 19),
-                Arguments.of("de/havox_design/aoc2021/day12/day12part1sample3.txt", 226)
+                Arguments.of("de/havox_design/aoc2021/day12/day12sample1.txt", 10),
+                Arguments.of("de/havox_design/aoc2021/day12/day12sample2.txt", 19),
+                Arguments.of("de/havox_design/aoc2021/day12/day12sample3.txt", 226)
             )
 
         @JvmStatic
         private fun getDataForTestProcessPart2(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2021/day12/day12part2sample.txt", 0L)
+                Arguments.of("de/havox_design/aoc2021/day12/day12sample1.txt", 36),
+                Arguments.of("de/havox_design/aoc2021/day12/day12sample2.txt", 103),
+                Arguments.of("de/havox_design/aoc2021/day12/day12sample3.txt", 3509)
             )
     }
 }
