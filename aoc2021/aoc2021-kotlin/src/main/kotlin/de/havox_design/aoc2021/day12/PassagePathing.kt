c8bec4e903639a609ee90a122b0beeeb0db34e90
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-05-25 16:43:00 +0200

    Solved AoC2021 Day 12 Part 2

--- aoc2021/aoc2021-kotlin/src/main/kotlin/de/havox_design/aoc2021/day12/PassagePathing.kt
+++ aoc2021/aoc2021-kotlin/src/main/kotlin/de/havox_design/aoc2021/day12/PassagePathing.kt
@@ -49,8 +49,60 @@ class PassagePathing(private var filename: String) {
         return finish.size
     }
 
-    fun processPart2(): Any =
-        0L
+    fun processPart2(): Any {
+        val edges = data
+            .map { it.split(DELIMITER_CONNECTION) }
+            .flatMap {
+                listOf(
+                    it.first() to it.last(),
+                    it.last() to it.first()
+                )
+            }
+            .groupBy(
+                keySelector = { it.first },
+                valueTransform = { it.second },
+            )
+            .mapValues { it.value.sorted() }
+
+        val visited = mutableSetOf<List<String>>()
+        val finish = mutableSetOf<List<String>>()
+        val queue = ArrayDeque(listOf(listOf(NODE_NAME_START) to false))
+
+        while (queue.isNotEmpty()) {
+            val (curr, twice) = queue.removeFirst()
+
+            if (curr !in visited) {
+                visited += curr
+
+                val tail = curr.last()
+                val nexts = edges[tail].orEmpty()
+
+                if (NODE_NAME_END in nexts) {
+                    finish += curr + NODE_NAME_END
+                }
+
+                queue += nexts
+                    .asSequence()
+                    .filter { it !in setOf(NODE_NAME_START, NODE_NAME_END) }
+                    .filter { it.uppercase() == it || it !in curr }
+                    .map { curr + it }
+                    .filter { it !in visited }
+                    .map { it to twice }
+
+                if (!twice) {
+                    queue += nexts
+                        .asSequence()
+                        .filter { it !in setOf(NODE_NAME_START, NODE_NAME_END) }
+                        .filter { it.lowercase() == it && it in curr }
+                        .map { curr + it }
+                        .filter { it !in visited }
+                        .map { it to true }
+                }
+            }
+        }
+
+        return finish.size
+    }
 
     private fun getResourceAsText(path: String): List<String> =
         this
