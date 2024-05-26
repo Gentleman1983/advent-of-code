f067c7ce272b32879982a7fde0c6795c65db14a6
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-05-26 13:41:10 +0200

    Added AoC2021 Day 18 Part 2

--- aoc2021/aoc2021-kotlin/src/test/kotlin/de/havox_design/aoc2021/day18/Day18Test.kt
+++ aoc2021/aoc2021-kotlin/src/test/kotlin/de/havox_design/aoc2021/day18/Day18Test.kt
@@ -15,20 +15,20 @@ class Day18Test {
 
     @ParameterizedTest
     @MethodSource("getDataForTestProcessPart2")
-    fun testProcessPart2(filename: String, expectedResult: Long) =
+    fun testProcessPart2(filename: String, expectedResult: Int) =
         Snailfish(filename).processPart2().shouldBe(expectedResult)
 
     companion object {
         @JvmStatic
         private fun getDataForTestProcessPart1(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2021/day18/day18part1sample.txt", 4140)
+                Arguments.of("de/havox_design/aoc2021/day18/day18sample.txt", 4140)
             )
 
         @JvmStatic
         private fun getDataForTestProcessPart2(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2021/day18/day18part2sample.txt", 0L)
+                Arguments.of("de/havox_design/aoc2021/day18/day18part2sample.txt", 3993)
             )
     }
 }
