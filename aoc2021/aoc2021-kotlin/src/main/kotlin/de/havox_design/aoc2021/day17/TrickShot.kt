d8217bf728fd0ada9db8e1877a74a96d02877bd5
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-05-26 11:11:04 +0200

    Solved AoC2021 Day 17 Part 2

--- aoc2021/aoc2021-kotlin/src/main/kotlin/de/havox_design/aoc2021/day17/TrickShot.kt
+++ aoc2021/aoc2021-kotlin/src/main/kotlin/de/havox_design/aoc2021/day17/TrickShot.kt
@@ -47,8 +47,41 @@ class TrickShot(private var filename: String) {
         return best
     }
 
-    fun processPart2(): Any =
-        0L
+    fun processPart2(): Any {
+        val (fromX, toX, fromY, toY) = PATTERN
+            .find(data)
+            ?.groupValues
+            .orEmpty()
+            .drop(1)
+            .map { it.toInt() }
+
+        return (0..toX)
+            .sumOf { x ->
+                (-100..100)
+                    .sumOf { y ->
+                        val positions = generateSequence(Position2d(0, 0) to Position2d(x, y)) { (pos, speed) ->
+                            val nextPos = pos + speed
+                            val nextSpeed = Position2d(
+                                x = speed.x - speed.x.sign,
+                                y = speed.y - 1,
+                            )
+
+                            when {
+                                speed.x == 0 && pos.y < fromY -> null
+                                else -> nextPos to nextSpeed
+                            }
+                        }
+                            .map { it.first }
+                            .toList()
+
+                        if (positions.any { it.x in fromX..toX && it.y in fromY..toY }) {
+                            1L
+                        } else {
+                            0L
+                        }
+                    }
+            }
+    }
 
     private operator fun Position2d<Int>.plus(other: Position2d<Int>): Position2d<Int> =
         Position2d(x + other.x, y + other.y)
--- aoc2021/aoc2021-kotlin/src/test/kotlin/de/havox_design/aoc2021/day17/Day17Test.kt
+++ aoc2021/aoc2021-kotlin/src/test/kotlin/de/havox_design/aoc2021/day17/Day17Test.kt
@@ -15,7 +15,7 @@ class Day17Test {
 
     @ParameterizedTest
     @MethodSource("getDataForTestProcessPart2")
-    fun testProcessPart2(filename: String, expectedResult: Int) =
+    fun testProcessPart2(filename: String, expectedResult: Long) =
         TrickShot(filename).processPart2().shouldBe(expectedResult)
 
     companion object {
@@ -28,7 +28,7 @@ class Day17Test {
         @JvmStatic
         private fun getDataForTestProcessPart2(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2021/day17/day17sample.txt", 112)
+                Arguments.of("de/havox_design/aoc2021/day17/day17sample.txt", 112L)
             )
     }
 }
