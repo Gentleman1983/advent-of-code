c1f95ca40965b69026ece18637c7a47767f90be2
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-05-26 19:51:57 +0200

    Solved AoC2021 Day 25 Part 1

--- aoc2021/aoc2021-kotlin/src/main/kotlin/de/havox_design/aoc2021/day25/SeaCucumber.kt
+++ aoc2021/aoc2021-kotlin/src/main/kotlin/de/havox_design/aoc2021/day25/SeaCucumber.kt
@@ -1,14 +1,34 @@
 package de.havox_design.aoc2021.day25
 
+import de.havox_design.aoc.utils.kotlin.model.positions.Position2d
+
 class SeaCucumber(private var filename: String) {
     private val data = getResourceAsText(filename)
 
-    fun processPart1(): Any =
-        0L
+    fun processPart1(): Any {
+        var map = Map(data.map { it.toList() })
+        var step = 0
+
+        do {
+            step += 1
+
+            val (eastCount, eastMap) = map.moveEast()
+            val (southCount, southMap) = eastMap.moveSouth()
+
+            map = southMap
+        } while (eastCount + southCount > 0)
+
+        return step
+    }
 
     fun processPart2(): Any =
         0L
 
     private fun getResourceAsText(path: String): List<String> =
-        this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
+        this
+            .javaClass
+            .classLoader
+            .getResourceAsStream(path)!!
+            .bufferedReader()
+            .readLines()
 }
