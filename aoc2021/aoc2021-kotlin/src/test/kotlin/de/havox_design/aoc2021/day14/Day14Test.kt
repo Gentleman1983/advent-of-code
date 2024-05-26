cf9826b8fc87bc31f8d0e53e14f1a08e3d73df9a
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-05-26 09:07:20 +0200

    Added AoC2021 Day 14 Part 2

--- aoc2021/aoc2021-kotlin/src/test/kotlin/de/havox_design/aoc2021/day14/Day14Test.kt
+++ aoc2021/aoc2021-kotlin/src/test/kotlin/de/havox_design/aoc2021/day14/Day14Test.kt
@@ -16,20 +16,20 @@ class Day14Test {
 
     @ParameterizedTest
     @MethodSource("getDataForTestProcessPart2")
-    fun testProcessPart2(filename: String, expectedResult: Long) =
+    fun testProcessPart2(filename: String, expectedResult: BigInteger) =
         ExtendedPolymerization(filename).processPart2().shouldBe(expectedResult)
 
     companion object {
         @JvmStatic
         private fun getDataForTestProcessPart1(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2021/day14/day14part1sample.txt", BigInteger.valueOf(1588))
+                Arguments.of("de/havox_design/aoc2021/day14/day14sample.txt", BigInteger.valueOf(1588))
             )
 
         @JvmStatic
         private fun getDataForTestProcessPart2(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2021/day14/day14part2sample.txt", 0L)
+                Arguments.of("de/havox_design/aoc2021/day14/day14sample.txt", BigInteger.valueOf(2188189693529))
             )
     }
 }
