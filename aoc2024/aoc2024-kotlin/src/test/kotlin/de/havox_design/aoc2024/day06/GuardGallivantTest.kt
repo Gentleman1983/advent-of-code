7ca240a4c8abbb57ef9b89b7246619f114aa05cb
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-12-06 16:32:40 +0100

    Added riddle for AoC 2024 day 6 part 2.

--- aoc2024/aoc2024-kotlin/src/test/kotlin/de/havox_design/aoc2024/day06/GuardGallivantTest.kt
+++ aoc2024/aoc2024-kotlin/src/test/kotlin/de/havox_design/aoc2024/day06/GuardGallivantTest.kt
@@ -15,20 +15,20 @@ class GuardGallivantTest {
 
     @ParameterizedTest
     @MethodSource("getDataForTestProcessPart2")
-    fun testProcessPart2(filename: String, expectedResult: Long) =
+    fun testProcessPart2(filename: String, expectedResult: Int) =
         GuardGallivant(filename).processPart2().shouldBe(expectedResult)
 
     companion object {
         @JvmStatic
         private fun getDataForTestProcessPart1(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2024/day06/day06part1sample.txt", 41)
+                Arguments.of("de/havox_design/aoc2024/day06/day06sample.txt", 41)
             )
 
         @JvmStatic
         private fun getDataForTestProcessPart2(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2024/day06/day06part2sample.txt", 0L)
+                Arguments.of("de/havox_design/aoc2024/day06/day06sample.txt", 6)
             )
     }
 }
