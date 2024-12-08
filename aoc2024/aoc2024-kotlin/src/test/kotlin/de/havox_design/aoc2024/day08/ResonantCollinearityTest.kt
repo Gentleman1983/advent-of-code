5cbfa7a5b9f9981e7460e82f3387d48fee96b86f
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-12-08 11:16:53 +0100

    Added riddle for AoC 2024 day 8 part 2.

--- aoc2024/aoc2024-kotlin/src/test/kotlin/de/havox_design/aoc2024/day08/ResonantCollinearityTest.kt
+++ aoc2024/aoc2024-kotlin/src/test/kotlin/de/havox_design/aoc2024/day08/ResonantCollinearityTest.kt
@@ -15,7 +15,7 @@ class ResonantCollinearityTest {
 
     @ParameterizedTest
     @MethodSource("getDataForTestProcessPart2")
-    fun testProcessPart2(filename: String, expectedResult: Long) =
+    fun testProcessPart2(filename: String, expectedResult: Int) =
         ResonantCollinearity(filename).processPart2().shouldBe(expectedResult)
 
     companion object {
@@ -28,7 +28,8 @@ class ResonantCollinearityTest {
         @JvmStatic
         private fun getDataForTestProcessPart2(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2024/day08/day08part2sample.txt", 0L)
+                Arguments.of("de/havox_design/aoc2024/day08/day08part2sample1.txt", 34),
+                Arguments.of("de/havox_design/aoc2024/day08/day08part2sample1.txt", 9)
             )
     }
 }
