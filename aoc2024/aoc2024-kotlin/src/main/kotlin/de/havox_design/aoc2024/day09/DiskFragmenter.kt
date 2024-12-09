206b97e971a7ab5e0d38b775892033b2272e9d2c
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-12-09 17:24:36 +0100

    Solved riddle for AoC 2024 day 9 part 2.

--- aoc2024/aoc2024-kotlin/src/main/kotlin/de/havox_design/aoc2024/day09/DiskFragmenter.kt
+++ aoc2024/aoc2024-kotlin/src/main/kotlin/de/havox_design/aoc2024/day09/DiskFragmenter.kt
@@ -50,8 +50,60 @@ class DiskFragmenter(private var filename: String) {
             .calculateCheckSum()
     }
 
-    fun processPart2(): Any =
-        0L
+    fun processPart2(): Any {
+        val memory = data
+            .toMutableList()
+        var indexToCheck = memory.count { it.type == Type.USED } - 1
+
+        while (indexToCheck > 0) {
+            val blockToMove = memory
+                .withIndex()
+                .filter { (_, block) ->
+                    block.type == Type.USED
+                }
+                .withIndex()
+                .last { (filteredIndex, globalIndexedBlock) ->
+                    val (_, block) = globalIndexedBlock
+                    block.type == Type.USED && filteredIndex == indexToCheck
+                }
+                .value
+
+            if (blockToMove.isNotMovable(memory)) {
+                indexToCheck--
+                continue
+            }
+
+            val emptyBlock = memory
+                .withIndex()
+                .first { (_, block) ->
+                    block.type == Type.FREE && block.length >= blockToMove.value.length
+                }
+
+            when {
+                emptyBlock.value.length == blockToMove.value.length -> {
+                    memory[blockToMove.index] = Block(null, Type.FREE, blockToMove.value.length)
+                    memory[emptyBlock.index] = blockToMove.value
+                }
+
+                emptyBlock.value.length > blockToMove.value.length -> {
+                    memory[blockToMove.index] = Block(null, Type.FREE, blockToMove.value.length)
+                    memory
+                        .removeAt(emptyBlock.index)
+                    memory
+                        .addAll(
+                        emptyBlock.index,
+                        listOf(
+                            blockToMove.value,
+                            Block(null, Type.FREE, emptyBlock.value.length - blockToMove.value.length)
+                        )
+                    )
+                }
+            }
+        }
+
+        return memory
+            .calculateCheckSum()
+    }
 
     private fun List<Block>.hasFreeSpace(): Boolean =
         any { it.type == Type.FREE }
@@ -73,6 +125,11 @@ class DiskFragmenter(private var filename: String) {
             .findAll(this)
             .map { it.value }
             .toList()
+
+    private fun IndexedValue<Block>.isNotMovable(memory: List<Block>): Boolean =
+        memory
+            .take(index)
+            .none { it.type == Type.FREE && it.length >= value.length }
 
     private fun parseInput(input: List<String>): List<Block> =
         input[0]
