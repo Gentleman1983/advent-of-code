61f387f9bd3b4f86d82c2c2fa469de2aaf25d7ee
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-12-15 11:36:07 +0100

    Added riddle for AoC 2024 day 15 part 2.

--- aoc2024/aoc2024-kotlin/src/test/kotlin/de/havox_design/aoc2024/day15/WarehouseWoesTest.kt
+++ aoc2024/aoc2024-kotlin/src/test/kotlin/de/havox_design/aoc2024/day15/WarehouseWoesTest.kt
@@ -15,7 +15,7 @@ class WarehouseWoesTest {
 
     @ParameterizedTest
     @MethodSource("getDataForTestProcessPart2")
-    fun testProcessPart2(filename: String, expectedResult: Long) =
+    fun testProcessPart2(filename: String, expectedResult: Int) =
         WarehouseWoes(filename).processPart2().shouldBe(expectedResult)
 
     companion object {
@@ -29,7 +29,7 @@ class WarehouseWoesTest {
         @JvmStatic
         private fun getDataForTestProcessPart2(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("de/havox_design/aoc2024/day15/day15part2sample.txt", 0L)
+                Arguments.of("de/havox_design/aoc2024/day15/day15part2sample.txt", 9021)
             )
     }
 }
