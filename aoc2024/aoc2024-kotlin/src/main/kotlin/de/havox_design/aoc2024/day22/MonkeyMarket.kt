41f5b91e5ed9f4871a4a3d0b4a04ef590e9712f4
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2024-12-22 16:32:10 +0100

    Solved riddle for AoC 2024 day 22 part 1.

--- aoc2024/aoc2024-kotlin/src/main/kotlin/de/havox_design/aoc2024/day22/MonkeyMarket.kt
+++ aoc2024/aoc2024-kotlin/src/main/kotlin/de/havox_design/aoc2024/day22/MonkeyMarket.kt
@@ -1,13 +1,46 @@
 package de.havox_design.aoc2024.day22
 
 class MonkeyMarket(private var filename: String) {
-    private val data = getResourceAsText(filename)
+    private val data = parseInput(getResourceAsText(filename))
 
     fun processPart1(): Any =
-        0L
+        data
+            .sumOf { number ->
+                (0..<MAX_SIZE)
+                    .fold(number) { acc, _ ->
+                        acc.nextSecretNumber()
+                    }
+            }
 
     fun processPart2(): Any =
         0L
+
+    private fun Long.mixInto(other: Long) =
+        this xor other
+
+    private fun Long.prune() =
+        this % MODULO_FACTOR
+
+    private fun Long.nextSecretNumber(): Long {
+        val step1 =
+            (this * 64)
+                .mixInto(this)
+                .prune()
+        val step2 =
+            (step1 / 32)
+                .mixInto(step1)
+                .prune()
+        val step3 =
+            (step2 * 2048)
+                .mixInto(step2)
+                .prune()
+
+        return step3
+    }
+
+    private fun parseInput(input: List<String>) =
+        input
+            .map(String::toLong)
 
     private fun getResourceAsText(path: String): List<String> =
         this
@@ -16,4 +49,9 @@ class MonkeyMarket(private var filename: String) {
             .getResourceAsStream(path)!!
             .bufferedReader()
             .readLines()
+
+    companion object {
+        private const val MAX_SIZE = 2000
+        private const val MODULO_FACTOR = 16777216L
+    }
 }
